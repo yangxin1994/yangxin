86cc492e3c83d5f776476709f38def6a975908fc
Jesse Yang <jesse.yang1985@gmail.com>
2012-08-28 09:39:05 +0800

    adjust error code enumeration

--- test/functional/admin/points_controller_test.rb
+++ test/functional/admin/points_controller_test.rb
@@ -4,15 +4,15 @@ class Admin::PointsControllerTest < ActionController::TestCase
     clear(User, PointLog)
     @admin_foo = FactoryGirl.create(:admin_foo)
     @user_bar = FactoryGirl.create(:user_bar)
+    @auth_key = sign_in('admin_foo@gmail.com', '123123123')
   end
 
   test "should add 100 point for user_bar" do
 
-    auth_key = sign_in('admin_foo@gmail.com', '123123123')
     re = post :operate, :format => :json,
          :operate_point => 100,
          :user_id => @user_bar.id,
-         :auth_key => auth_key
+         :auth_key => @auth_key
     #pp re
     pp PointLog.count
     @user_bar = User.find(@user_bar.id)
