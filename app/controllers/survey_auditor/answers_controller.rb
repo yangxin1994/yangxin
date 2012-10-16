b58779a0cfc6ca48a08a35da392b2582f55dcf09
Karma <boris.isolet@gmail.com>
2012-10-16 09:16:00 +0800

    export & import actions added

---
+++ app/controllers/survey_auditor/answers_controller.rb
@@ -0,0 +1,28 @@
+# encoding: utf-8
+require 'error_enum'
+class SurveyAuditor::AnswersController < SurveyAuditor::ApplicationController
+	
+	def csv_header
+		survey = Survey.find_by_id params[:survey_id]
+		respond_and_render_json(survey.is_valid?) do
+			if is_success
+				survey.get_csv_header
+			else
+				survey.as_retval
+			end
+		end
+	end
+
+	def import_answer
+		survey = Survey.find_by_id params[:survey_id]
+		respond_and_render_json(survey.is_valid?) do
+			if is_success
+				survey.answer_import(params[:csv])
+			else
+				survey.as_retval
+			end
+		end
+
+	end
+
+end
