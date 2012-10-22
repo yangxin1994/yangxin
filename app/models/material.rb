e384610db36470f3b45b2240fa368031adce783f
Karma <boris.isolet@gmail.com>
2012-10-22 08:54:23 +0800

    add  Material[picture_url]

--- app/models/material.rb
+++ app/models/material.rb
@@ -3,24 +3,26 @@ require 'securerandom'
 
 class Material
 	include Mongoid::Document
+	include Mongoid::Timestamps
 	# 1 for image, 2 for video, 4 for audio
 	field :material_type, :type => Integer
 	field :title, :type => String
 	field :value, :type => String
-	field :created_at, :type => Integer
+	field :picture_url, :type => String
 
 	belongs_to :user
+
+	default_scope ->(o = 'ASC'){ order_by("created_at", o) }
 
 	def self.find_by_id(material_id)
 		return Material.where(:_id => material_id).first
 	end
 
 	def self.check_and_create_new(current_user, material)
-		return ErrorEnum::WRONG_MATERIAL_TYPE if ![1, 2, 4].include?(material["material_type"].to_i)
+		return ErrorEnum::WRONG_MATERIAL_TYPE unless [1, 2, 4].include?(material["material_type"].to_i)
 		material_inst = Material.new(:material_type => material["material_type"].to_i, 
 			:value => material["value"], 
-			:title => material["title"], 
-			:created_at => Time.now.to_i)
+			:title => material["title"])
 		material_inst.save
 		current_user.materials << material_inst
 		current_user.save
