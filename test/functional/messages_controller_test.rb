1b046789a50d2d283207e50591a5118075b76559
Karma <boris.isolet@gmail.com>
2012-11-02 17:41:33 +0800

    fix messages_c_test

---
+++ test/functional/messages_controller_test.rb
@@ -0,0 +1,27 @@
+require 'test_helper'
+
+class MessagesControllerTest < ActionController::TestCase
+	setup do
+		clear(Message, User)
+		@user_bar = FactoryGirl.create(:user_bar)
+		@user_bar.update_attribute(:last_read_messeges_time, Time.now - 1.day)
+		@auth_key = sign_in('user_bar@gmail.com', '123123123')
+		3.times{ FactoryGirl.create(:message) }
+	end
+
+	test "should show a messages list" do
+		get :index, :format => :json, page: 0, per_page: 2, :auth_key => @auth_key
+		pp @user_bar.unread_messages_count
+		pp response.body
+		get :index, :format => :json, :auth_key => @auth_key
+		pp response.body
+		assert response.body
+	end
+
+	test "should show unread messages count" do
+		get :unread_count, :format => :json, :auth_key => @auth_key
+		#pp @user_bar.unread_messages_count
+		#pp response.body
+		assert_equal "{\"value\":3,\"success\":true}", response.body
+	end 	
+end
