c815ded70c8431edd40fe33c313eeb1e38508dba
Karma <boris.isolet@gmail.com>
2012-11-02 17:50:11 +0800

    strip messages_c

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,7 +4,7 @@ class MessagesController < ApplicationController
   before_filter :require_sign_in
 
   def index
-    @messages = current_user.show_messages.slice(current_page, per_page)
+    @messages = current_user.show_messages.slice((page - 1) * per_page, per_page)
     #@messages = ErrorEnum::MESSAGE_NOT_FOUND if @messages.empty? 
     respond_and_render_json { @messages }
   end
@@ -12,8 +12,5 @@ class MessagesController < ApplicationController
   def unread_count
     respond_and_render_json { current_user.unread_messages_count }
   end 
-  private
-  def current_page
-    (page - 1) * per_page
-  end
+
 end
