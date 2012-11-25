cd9ef670d4cdab519c6f22f469cb34066c1906be
shatle <shatle@qq.com>
2012-11-25 16:13:04 +0800

    update for adding not_review_answer_num field

--- app/controllers/answer_auditor/surveys_controller.rb
+++ app/controllers/answer_auditor/surveys_controller.rb
@@ -4,13 +4,21 @@ class AnswerAuditor::SurveysController < AnswerAuditor::ApplicationController
 
 	def index
 		if @current_user.is_admin
-			render_json_auto auto_paginate(Survey.where(
+			@surveys = Survey.where(
 					user_attr_survey: false, 
 					:new_survey => false, 
 					:publish_status.gt => 2
-				))
+				)
 		else
-			render_json_auto auto_paginate(@current_user.answer_auditor_allocated_surveys)
+			@surveys = @current_user.answer_auditor_allocated_surveys
 		end
+
+		@show_surveys = @surveys.page(page).per(per_page)
+		@show_surveys = @show_surveys.to_a.map{|elem| 
+			elem['not_review_answer_num']= elem.answers.where(status: 2, finish_type: 0).count
+			elem
+		}
+
+		render_json_auto (auto_paginate(@show_surveys, @surveys.count){@show_surveys}) and return
 	end
 end
