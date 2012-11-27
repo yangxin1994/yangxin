4185ac2c9e65d393b8444033ca624f1b35e3c43a
oliverhuang <oliverhdh@gmail.com>
2012-11-27 17:06:09 +0800

    update

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -6,7 +6,7 @@ class UserMailer < ActionMailer::Base
 	def welcome_email(user, callback)
 		@user = user
 		activate_info = {"email" => user.email, "time" => Time.now.to_i}
-		@activate_link = "http://" + OOPSDATA[RailsEnv.get_rails_env]["root_url"] + "/#{callback}?key=" + CGI::escape(Encryption.encrypt_activate_key(activate_info.to_json))
+		@activate_link = "#{callback}?key=" + CGI::escape(Encryption.encrypt_activate_key(activate_info.to_json))
 		mail(:to => user.email, 
 					:subject => "欢迎注册Oops!Data",
 					:content_type => "text/html; charset=utf-8")
@@ -15,7 +15,7 @@ class UserMailer < ActionMailer::Base
 	def activate_email(user, callback)
 		@user = user
 		activate_info = {"email" => user.email, "time" => Time.now.to_i}
-		@activate_link = "http://" + OOPSDATA[RailsEnv.get_rails_env]["root_url"] + "/#{callback}?key=" + CGI::escape(Encryption.encrypt_activate_key(activate_info.to_json))
+		@activate_link = "#{callback}?key=" + CGI::escape(Encryption.encrypt_activate_key(activate_info.to_json))
 		mail(:to => user.email, 
 					:subject => "激活Oops!Data",
 					:content_type => "text/html; charset=utf-8")
@@ -24,7 +24,7 @@ class UserMailer < ActionMailer::Base
 	def password_email(user, callback)
 		@user = user
 		password_info = {"email" => user.email, "time" => Time.now.to_i}
-		@password_link = "http://" + OOPSDATA[RailsEnv.get_rails_env]["root_url"] + "/#{callback}?key=" + CGI::escape(Encryption.encrypt_activate_key(password_info.to_json))
+		@password_link = "#{callback}?key=" + CGI::escape(Encryption.encrypt_activate_key(password_info.to_json))
 		mail(:to => user.email, 
 					:subject => "重置Oops!Data密码",
 					:content_type => "text/html; charset=utf-8")
