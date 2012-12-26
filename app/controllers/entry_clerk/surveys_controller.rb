18165e5cbc1636057b11bc4c7d88bfe7d1955e43
Karma <boris.isolet@gmail.com>
2012-12-26 16:17:49 +0800

    update

--- app/controllers/entry_clerk/surveys_controller.rb
+++ app/controllers/entry_clerk/surveys_controller.rb
@@ -4,24 +4,26 @@ require 'error_enum'
 class EntryClerk::SurveysController < EntryClerk::ApplicationController
 	
 	def csv_header
-		render_json(survey = Survey.find_by_id params[:survey_id]) do |s|
+		survey = Survey.find_by_id(params[:id])
+		render_json !!survey do |s|
 			if s
 				survey.csv_header
 			else
 				{ :error_code => ErrorEnum::SURVEY_NOT_EXIST, 
-						 :error_message => "问卷不存在"}
+				  :error_message => "survey not exist"}
 			end
 		end
 	end
 
 	def import_answer
-		render_json(survey = Survey.find_by_id params[:survey_id]) do |s|
+		survey = Survey.find_by_id(params[:id])
+		render_json !!survey do |s|
 			if s
 				unless survey.answer_import(params[:csv])
 					@is_success = false
 					{
 						:error_code => ErrorEnum::WRONG_ANSWERS,
-						:error_message => "答案有误"
+						:error_message => "wrong answers"
 					}
 				end
 			else
