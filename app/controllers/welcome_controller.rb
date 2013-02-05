5865692be6df7fe674b6b97cc028c86ba1b0711d
JesseYang <jesse.yang1985@gmail.com>
2013-02-05 09:50:06 +0800

    file upload

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,10 +1,18 @@
 class WelcomeController < ApplicationController
 
 	def index
+
+		path = 'public/data'
+		File.open(path, "wb") { |f| f.write(params[:file].read) }
+
 		render :text => params.inspect and return
 		if user_signed_in?
 			redirect_to home_path and return
 		end
+	end
+
+	def upload
+		
 	end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -475,6 +475,8 @@ OopsData::Application.routes.draw do
 	# just remember to delete public/index.html.
 	root :to => 'welcome#index'
 
+	match '/upload' => 'welcome#upload'
+
 	# See how all your routes lay out with "rake routes"
 
 	# This is a legacy wild controller route that's not recommended for RESTful applications.
