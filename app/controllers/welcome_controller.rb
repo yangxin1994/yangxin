338df96596bf6a7e2377c55dd9b5ac0e15a79f13
JesseYang <jesse.yang1985@gmail.com>
2013-02-16 11:37:32 +0800

    test oops_deploy

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,7 +3,6 @@ require 'quill_common'
 class WelcomeController < ApplicationController
 
 	def index
-		
 		render :text => QuillCommon::AddressUtility.find_region_code_by_latlng(33.578015, 87.495117) and return
 		render :text => params and return
 		
