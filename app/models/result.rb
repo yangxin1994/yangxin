396eeaae7afc9ff1667c97646d3426202d7fb66e
JesseYang <jesse.yang1985@gmail.com>
2013-02-19 18:55:58 +0800

    fix report export progress showing bug

--- app/models/result.rb
+++ app/models/result.rb
@@ -44,7 +44,8 @@ class Result
 		return result.status if result && result.status == 1 || result.status == -1
 
 		# the task has not been finished, check the progress
-		task = TaskClient.get_task(result.task_id)
+		task_id = result.task_id
+		task = TaskClient.get_task(task_id)
 
 		return ErrorEnum::TASK_NOT_EXIST if task == ErrorEnum::TASK_NOT_EXIST
 		progress = task["progress"]
