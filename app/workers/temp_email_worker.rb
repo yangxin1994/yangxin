a8b7d7503c5c62d2b582ce8ab564d0efc1bdcbb0
JesseYang <jesse.yang1985@gmail.com>
2013-03-19 17:30:02 +0800

    adjust temp email

--- app/workers/temp_email_worker.rb
+++ app/workers/temp_email_worker.rb
@@ -2,10 +2,14 @@ class TempEmailWorker
 	include Sidekiq::Worker
 	sidekiq_options :retry => false, :queue => "oopsdata_#{Rails.env}".to_sym
 
-	def perform(index)
+	def perform(index, digit)
 		s_id_ary = ["5142b0de408c9950a9000030", "514661c9408c99fcd700001e"]
+		import_emails = ImportEmail.all.to_a
 		while true
-			import_email = ImportEmail.not_sent.shuffle[0]
+			import_email = import_emails[digit]
+			digit += 10
+			return if import_email.nil?
+			next if import_email.sent
 			import_email.sent = true
 			import_email.save
 			begin
