9a37097a91e04442d95827309f0f9f179c530a62
JesseYang <jesse.yang1985@gmail.com>
2013-03-24 20:43:00 +0800

    remove bounced emails

---
+++ app/workers/remove_bounced_email_worker.rb
@@ -0,0 +1,8 @@
+class RemoveBouncedEmailWorker
+	include Sidekiq::Worker
+	sidekiq_options :retry => false, :queue => "oopsdata_#{Rails.env}".to_sym
+	def perform
+		ImportEmail.remove_bounce_emails
+		return true
+	end
+end
