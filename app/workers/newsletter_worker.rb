b37e5d1829dc6bb6817dfd477b3ba478916f4969
Karma <boris.isolet@gmail.com>
2013-04-19 17:58:19 +0800

    update

--- app/workers/newsletter_worker.rb
+++ app/workers/newsletter_worker.rb
@@ -9,9 +9,13 @@ class NewsletterWorker
     Subscriber.subscribed.each do |sub|
       return if '1' != Sidekiq.redis{|r| r.get('news_flag')}
       next if sub.newsletters.include? newsletter
-      NewsletterMailer.news_email(newsletter, content_html, sub).deliver
-      sub.newsletters << newsletter
-      sub.save
+      begin
+        NewsletterMailer.news_email(newsletter, content_html, sub).deliver
+        sub.newsletters << newsletter
+        sub.save
+      rescue
+        p "发送出错"
+      end
     end
     newsletter.status = 1 #设置发送完成标识
     newsletter.save
