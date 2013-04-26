52210db5506d40cc8f35f9283030fb9c43850dc5
karmaQ <boris.isolet@gmail.com>
2013-04-26 16:32:58 +0800

    Update subscribers_controller.rb

--- app/controllers/admin/subscribers_controller.rb
+++ app/controllers/admin/subscribers_controller.rb
@@ -4,7 +4,7 @@ class Admin::SubscribersController < Admin::ApplicationController
   before_filter :require_sign_in
 
   def index
-    render_json Subscriber.page(page).present_json(:admin))
+    render_json Subscriber.page(page).present_json(:admin)
   end
 
   def_each :unsubscribed, :subscribed do |method_name|
