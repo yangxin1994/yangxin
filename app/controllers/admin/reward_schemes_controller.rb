43b150fdc500a589a07c2b953d382d9da42bf5be
JesseYang <jesse.yang1985@gmail.com>
2013-06-14 14:13:07 +0800

    code review

--- app/controllers/admin/reward_schemes_controller.rb
+++ app/controllers/admin/reward_schemes_controller.rb
@@ -1,31 +1,36 @@
 require 'error_enum'
 
-class RewardSchemesController < ApplicationController
-	before_filter :require_sign_in
+class Admin::RewardSchemesController < Admin::ApplicationController
+	before_filter :check_survey_existence
+
+	def check_survey_existence
+		@survey = Survey.find_by_id(params[:survey_id])
+		render_json_auto(ErrorEnum::SURVEY_NOT_EXIST) and return if @survey.nil?
+	end
 
 	def index
-		reward_scheme = Survey.reward_schemes   ##Return a Array, [] means find nothing
-		render_json_auto( auto_paginate(result) )  and return
+		reward_schemes = @survey.reward_schemes   ##Return a Array, [] means find nothing
+		render_json_auto( auto_paginate(reward_schemes) )  and return
 	end
 
 	def show
-		reward_scheme = RewardScheme.find_by_id(:id)
+		reward_scheme = RewardScheme.find_by_id(params[:id])
 		render_json_auto(reward_scheme)
 	end
 
 	def create
-		retval = RewardScheme.create_reward_scheme(params[:reward_scheme_id], params[:reward_scheme_setting])
+		retval = RewardScheme.create_reward_scheme(@survey, params[:reward_scheme_setting])
 		render_json_auto(retval) and return
 	end
 
 	def update
-		retval = RewardScheme.update_review_scheme(params[:reward_scheme_id], params[:reward_scheme_setting])
+		retval = RewardScheme.update_review_scheme(params[:id], params[:reward_scheme_setting])
 		format.json	{ render_json_auto(retval) and return }
 	end
 
 	def destory
 		reward_scheme = RewardScheme.find_by_id(params[:id])
-		retval = (reward_scheme == nil ? ErrorEnum::REWARD_SCHEME_NOT_EXIST : reward_scheme.destory)
+		retval = (reward_scheme.nil? ? ErrorEnum::REWARD_SCHEME_NOT_EXIST : reward_scheme.destory)
 		render_json_auto(retval) and return
 	end
 
