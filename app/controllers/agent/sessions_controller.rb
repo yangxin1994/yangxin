ddc67d1e2efc2d926332076e9275ad2a92f0cc97
JesseYang <jesse.yang1985@gmail.com>
2013-07-02 09:45:09 +0800

    tidy up code

---
+++ app/controllers/agent/sessions_controller.rb
@@ -0,0 +1,28 @@
+# encoding: utf-8
+require 'encryption'
+require 'error_enum'
+require 'tool'
+class Agent::SessionsController < Agent::ApplicationController
+
+	before_filter :require_agent_task, :only => [:reset_password]
+
+	def create
+		login = AgentTask.login(params[:agent_task]["email"], params[:agent_task]["password"], params[:survey_id])
+		render_json_auto(login) and return
+	end
+
+	def destroy
+		AgentTask.logout(params[:id])
+		render_json_s and return
+	end
+
+	def reset_password
+		retval = @agent_task.reset_password(params[:old_password], params[:new_password])
+		render_json_auto(retval) and return
+	end
+
+	def login_with_auth_key
+		retval = AgentTask.login_with_auth_key(params[:id])
+		render_json_auto(retval) and return
+	end
+end
