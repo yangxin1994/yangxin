1986f01f9b9fbef040ce7d3671308d9c6a5911ec
JesseYang <jesse.yang1985@gmail.com>
2013-07-24 10:53:39 +0800

    删除消息

--- app/models/message_related/message.rb
+++ app/models/message_related/message.rb
@@ -20,6 +20,8 @@ class Message
 	# 1 the message is sent to special users
 	field :type, :type => Integer, default: 0
 
+	default_scope order_by(:created_at.desc)
+
 	belongs_to :sender, :class_name => "User", :inverse_of => :sended_messages
 	has_and_belongs_to_many :receiver, class_name: "User", inverse_of: :messages
 
