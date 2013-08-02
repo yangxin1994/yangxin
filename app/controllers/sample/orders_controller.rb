f09bb24d876a24ecbf7d4d1d75217e30b3e609c2
JesseYang <jesse.yang1985@gmail.com>
2013-08-02 14:49:18 +0800

    fix bug

--- app/controllers/sample/orders_controller.rb
+++ app/controllers/sample/orders_controller.rb
@@ -27,7 +27,7 @@ class Sample::OrdersController < ApplicationController
 		if params['order']['info_sys'].to_s == 'true'
 			@current_user.set_receiver_info(opt)
 		end		 	
-		return ErrorEnum::INVALID_GIFT_ID  unless gift_id
-		render_json_auto Order.create_redeem_order(@current_user._id,gift_id,amount,point,opt)
+		render_json_e ErrorEnum::INVALID_GIFT_ID and return unless gift_id
+		render_json_auto Order.create_redeem_order(@current_user._id,gift_id,amount,point,opt) and return
 	end
 end
