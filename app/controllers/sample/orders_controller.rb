4b15bef80ccff0675f76d9a16073c87f017589fc
JesseYang <jesse.yang1985@gmail.com>
2013-08-05 15:20:09 +0800

    调整订单显示顺序

--- app/controllers/sample/orders_controller.rb
+++ app/controllers/sample/orders_controller.rb
@@ -3,7 +3,7 @@ class Sample::OrdersController < ApplicationController
 	before_filter :require_sign_in
 
 	def index
-		@orders = @current_user.orders.where(:source => params[:source])
+		@orders = @current_user.orders.where(:source => params[:source]).desc(:created_at)
 		@orders_list = auto_paginate(@orders) do |paginated_orders|
 			paginated_orders.map { |e| e.info_for_sample }
 		end
