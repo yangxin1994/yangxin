5b27e4a338a9cbd8a577ebb50605dcfc9b4516e7
JesseYang <jesse.yang1985@gmail.com>
2013-08-13 14:02:23 +0800

    短链接生成接口

--- app/controllers/short_urls_controller.rb
+++ app/controllers/short_urls_controller.rb
@@ -1,8 +1,8 @@
 require 'error_enum'
 class ShortUrlsController < ApplicationController
 	
-	def show
-		retval = {"short_url" => MongoidShortener.generate(params[:id]) }
+	def create
+		retval = {"short_url" => MongoidShortener.generate(params[:link]) }
 		render_json_auto retval and return
 	end
 end
