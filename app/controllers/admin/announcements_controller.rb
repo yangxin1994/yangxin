b4dd3656fd76485ddd6b79346b09aa87bf12bea6
Karma <boris.isolet@gmail.com>
2013-08-22 17:29:17 +0800

    added orders search

--- app/controllers/admin/announcements_controller.rb
+++ app/controllers/admin/announcements_controller.rb
@@ -16,6 +16,10 @@ class Admin::AnnouncementsController < Admin::AdminController
     @announcement = PublicNotice.find params[:id]
   end
 
+  def new
+    @announcement ={}
+  end
+
   # POST
   def create
     params[:type] = params[:type].to_i
