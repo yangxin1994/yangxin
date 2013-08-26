b8d3c7b543385f234ad2de8b0ffc6bef5dd1f48c
Karma <boris.isolet@gmail.com>
2013-08-26 09:42:25 +0800

    agent

--- app/controllers/agent/agents_controller.rb
+++ app/controllers/agent/agents_controller.rb
@@ -4,12 +4,9 @@ class Agent::AgentsController < ApplicationController
   
   layout "agent-todc"
 
-  # because only user_id of user is from return data in many times.
-  # so add get_email method to replace user_id to email by AJAX.
-  before_filter :require_agent, :except => [:get_email]
+  before_filter :require_agent
 
   def require_agent
-    # if !is_admin  # is_admin only check admin and super admin.
     session[:auth_key] == current_agent.try('auth_key')
   end
 
@@ -23,11 +20,6 @@ class Agent::AgentsController < ApplicationController
       redirect_to "/agent/signin", :notice => "您需要登录才能继续操作!" and return
     end
     @current_agent
-  end
-
-  def get_email
-    _sign_out and return if session[:role].to_s.to_i==0
-    render :json => BaseClient.new(session_info, "/users")._get({}, "/#{params[:id]}/get_email")
   end
 
   # just render json with variable @result
