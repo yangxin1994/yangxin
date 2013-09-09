d01dcc4325a599866246601dd721d757b50075b2
Davis Gao <naitnix@126.com>
2013-09-09 19:01:21 +0800

    modified

--- app/models/log_related/regist_log.rb
+++ app/models/log_related/regist_log.rb
@@ -6,4 +6,9 @@ class RegistLog < Log
 	def self.create_regist_log(sample_id)
 		self.create(:user_id => sample_id)
 	end
+
+	def self.find_by_user_id(user_id)
+		self.where(:user_id => user_id).first
+	end
+
 end
