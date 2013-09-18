fcb6fb0735fd3908798583ba27293806249998db
Davis Gao <naitnix@126.com>
2013-09-18 17:52:17 +0800

    modified

--- app/assets/javascripts/pages/sample/surveys-bundle.js
+++ app/assets/javascripts/pages/sample/surveys-bundle.js
@@ -3,39 +3,10 @@
 //=require ui/widgets/od_subscribe
 jQuery(function($) {
 
-    var redict_to_subscribe = window.location.href.split('#')[1];
-    if (typeof(redict_to_subscribe) != 'undefined' && redict_to_subscribe != null) {
-
-        (function myLoop(i) {
-            setTimeout(function() {
-                if (i % 2 == 0) {
-                    $('div.content').css('background', 'yellow');
-                } else {
-                    $('div.content').css('background', 'white');
-                }
-                if (--i) myLoop(i);
-            }, 500)
-        })(11);
-
-    }
-
-
-
-    $('div.test').click(function() {
-        $(this).odSubscribe({
-            email_mobile: '15111427577',
-            btn: $(this)
-        })
-    })
-
-
-
     //如果用户已经关闭了安装插件提示，那么24小时内不再提示安装
     if ($.cookie('ignore_plugin')) {
         $('div.hot-research-banner').remove();
     }
-
-    //$('div.content').effect("highlight", {color: 'blue'}, 3000)
 
     //用户点击关闭安装插件提示，生成cookie，记录用户决定
     $('.hot-research-banner .popup-close').click(function() {
@@ -94,71 +65,14 @@ jQuery(function($) {
         window.location.href = "/surveys?status=" + status + "&reward_type=" + reward_type + "&answer_status=" + answer_status
     })
 
-    //关闭弹出框
-    $('button.close_f').live('click', function() {
-        $.fancybox.close();
-    })
-
-    //现在激活邮箱 button
-    $('button.mail_act_now').click(function() {
-        var link = $(this).attr('href');
-        link = 'http://www.' + link;
-        window.location.href = link;
-    })
-
-    //重新发送邮箱激活链接
-    $('a.re_mail').live('click', function() {
-        var mail = $('span.mail').text();
-        re_generate_email_activate($(this), mail);
-    })
-    //重新发送手机验证码
-    // $('a.re_mobile').live('click', function() {
-    //     var mobi = $('span.mobi').text();
-    //     $('div.error').remove();
-    //     re_generate_email_activate($(this), mobi);
-    // })
-
-    //手机验证
-    $('button.next_f').live('click', function() {
-        var butt = $(this);
-        var channel = $('.mobi').text();
-        var code = $('input[name="code"]').val()
-        if (code.length > 5) {
-            $.ajax({
-                type: "get",
-                url: '/surveys/mobile_rss_activate',
-                data: {
-                    rss_channel: channel,
-                    code: code
-                },
-                beforeSend: function() {
-                    butt.next('span').remove();
-                },
-                success: function(retval) {
-                    butt.next('span').remove();
-                    if (retval.success) {
-                        popup('#mobile_finish', null, null)
-                    } else {
-                        butt.next('.code_exp').show();
-                    }
-
-                }
-            })
-        } else {
-            $('input[name="code"]').focus();
-        }
-
-    })
-
-    $('input[name="code"]').focus(function() {
-        $('button.next_f').next('.code_exp').hide();
-    });
-
     //订阅按钮
     $('input[name="contact"]').next('a').click(function() {
         var channel = $('input[name="contact"]').val();
         if ($.regex.isEmail(channel) || $.regex.isMobile(channel)) {
-            make_rss_activate(channel, $(this))
+            $.od.odSubscribe({
+                email_mobile: channel,
+                btn: $(this)
+            })
         } else {
             $('input[name="contact"]').prev('.channel_err').show();
         }
@@ -167,119 +81,4 @@ jQuery(function($) {
     $('input[name="contact"]').focus(function() {
         $(this).prev('.channel_err').hide();
     })
-
-
-    //重新发送激活链接或激活码
-
-    function re_generate_email_activate(obj, email) {
-        $.ajax({
-            type: "POST",
-            url: '/surveys/generate_rss_activate_code',
-            data: {
-                rss_channel: email
-            },
-
-            beforeSend: function() {
-                obj.next('img').remove();
-                if (obj.next('img').length < 1) {
-                    if ($.regex.isEmail(email)) {
-                        obj.after('<img class="loading" src="/assets/image/sample/fancybox_loading@2x.gif" width="16" height="16" style="position:absolute;left:64px;top:44px;" />')
-                    } else {
-                        obj.after('<img class="loading" src="/assets/image/sample/fancybox_loading@2x.gif" width="16" height="16" style="position:absolute;right:50px;top:8px;" />')
-                    }
-
-                }
-            },
-            success: function(retval) {
-                obj.next('img').remove();
-                if ($.regex.isEmail(email)) {
-                    obj.after('<img class="loading" src="/assets/od-quillme/success.png" width="16" height="16" style="position:absolute;left:64px;top:44px;" />')
-                } else {
-                    obj.after('<img class="loading" src="/assets/od-quillme/success.png" width="16" height="16" style="position:absolute;rigt:50px;top:9px;" />')
-                }
-            }
-        });
-    }
-
-    //发送手机验证码或者邮箱激活链接
-
-    // function make_rss_activate(channel, button) {
-    //     $.ajax({
-    //         type: "POST",
-    //         url: '/surveys/generate_rss_activate_code',
-    //         data: {
-    //             rss_channel: channel
-    //         },
-    //         beforeSend: function() {
-    //             button.html('').append('<img style="margin-top:2px;" src="/assets/od-quillme/rss_loading.gif">').addClass('disabled')
-    //         },
-    //         success: function(retval) {
-    //             if (retval['success']) {
-    //                 if (retval['new_user']) {
-    //                     if ($.regex.isEmail(channel)) {
-    //                         if (channel.indexOf('gmail.com') > -1) {
-    //                             var mail_to = 'http://gmail.com';
-    //                         } else if (channel.indexOf('@tencent.') > -1) {
-    //                             var mail_to = 'http://mail.qq.com';
-    //                         } else if (channel.indexOf('@qq.') > -1) {
-    //                             var mail_to = 'http://mail.qq.com';
-    //                         } else {
-    //                             var mail_to = 'http://mail.' + channel.split('@')[1];
-    //                         }
-    //                         //邮件订阅成功提示页
-    //                         popup('#mail_success', channel, mail_to)
-    //                     } else {
-    //                         popup('#mobile_success', channel, null)
-    //                     }
-
-    //                 } else {
-    //                     if ($.regex.isEmail(channel)) {
-    //                         popup('#email_finish', null, null)
-    //                     } else {
-    //                         popup('#mobile_finish', null, null)
-    //                     }
-
-    //                 }
-    //             } else {
-    //                 //订阅过程出错提示页
-    //                 popup('#rss_error', null, null)
-    //             }
-    //         }
-    //     });
-    // }
-
-
-    //共用的弹出框函数
-
-    // function popup(obj, channel, mail_to) {
-    //     $.fancybox.open([{
-    //         href: obj
-    //     }], {
-    //         beforeShow: function() {
-    //             $(".fancybox-skin").css({
-    //                 "backgroundColor": "#fff"
-    //             });
-    //             if ($('span.mail').length > 0) {
-    //                 $('span.mail').text(channel)
-    //                 $('button').attr('href', mail_to)
-    //             }
-
-    //             if ($('span.mobi').length > 0) {
-    //                 $('span.mobi').text(channel)
-    //             }
-
-    //             $('a.re_mail').next('span').remove();
-    //             $('a.re_mobile').next('span').remove();
-
-    //         },
-    //         afterClose: function() {
-    //             $('a.rss-btn').html('订阅').removeClass('disabled');
-    //             $('input[name="code"]').val('');
-    //             $('.code_exp').hide();
-    //         },
-    //         width: 500,
-    //         height: 180,
-    //         scrolling: 'no'
-    //     });
-    // }
 });
--- app/assets/javascripts/ui/widgets/od_subscribe.js
+++ app/assets/javascripts/ui/widgets/od_subscribe.js
@@ -3,6 +3,9 @@
 //=require ui/plugins/od_enter
 //=require ./_templates/od_subscribe
 (function($) {
+    $('input, textarea').placeholder();
+
+    var $this = null;
     $.odWidget('odSubscribe', {
         options: {
             email_mobile: null,
@@ -10,18 +13,12 @@
         },
         _create: function() {
             this.element = this.hbs(this.options);
-
+            $this = this;
             if (this.options.email_mobile) {
                 this._make_rss_activate(this.options.email_mobile, this.options.btn, this);
             } else {
-                this._popup('#fill_email_mobile');
+                this._popup($this._find('#fill_email_mobile'));
             }
-
-            console.log("--------------------------")
-            console.log(this._find('a.re_mobile'))
-            this._find('a.re_mobile').addClass('ddd')
-            console.log("--------------------------")
-
         },
         _make_rss_activate: function(channel, button, obj) {
             $.ajax({
@@ -32,7 +29,7 @@
                 },
                 beforeSend: function() {
                     if (button) {
-                        button.html('').append('<imgstyle="margin-top:2px;"src="/assets/od-quillme/rss_loading.gif">').addClass('disabled')
+                        button.html('').append('<img style="margin-top:2px;"src="/assets/od-quillme/rss_loading.gif">').addClass('disabled')
                     }
                 },
                 success: function(retval) {
@@ -49,50 +46,137 @@
                                     var mail_to = 'http://mail.' + channel.split('@')[1];
                                 }
                                 //邮件订阅成功提示页
-                                obj._popup('#mail_success', channel, mail_to)
+                                obj._popup($this._find('#mail_success'), channel, mail_to)
                             } else {
-                                obj._popup('#mobile_success', channel, null)
+                                obj._popup($this._find('#mobile_success'), channel, null)
                             }
                         } else {
                             if ($.regex.isEmail(channel)) {
-                                obj._popup('#email_finish')
+                                obj._popup($this._find('#email_finish'))
                             } else {
-                                obj._popup('#mobile_finish')
+                                obj._popup($this._find('#mobile_finish'))
                             }
 
                         }
                     } else {
                         //订阅过程出错提示页
-                        obj._popup('#rss_error', null, null)
+                        obj._popup($this._find('#rss_error'))
                     }
                 }
             });
         },
+        _mobile_rss_activate: function() {
+            var butt = $('button.next_f');
+            var channel = $('.mobi').text();
+            var code = $('input[name="code"]').val()
+            if (code.length > 5) {
+                $.ajax({
+                    type: "get",
+                    url: '/surveys/mobile_rss_activate',
+                    data: {
+                        rss_channel: channel,
+                        code: code
+                    },
+                    beforeSend: function() {
+                        butt.next('span').remove();
+                    },
+                    success: function(retval) {
+                        butt.next('span').remove();
+                        if (retval.success) {
+                            $this._popup($this._find('#mobile_finish'))
+                        } else {
+                            butt.next('.code_exp').show();
+                        }
+                    }
+                })
+            } else {
+                $('input[name="code"]').focus();
+            }
+        },
+        _fill_email_mobile: function() {
+            email_mobile = $('input.email_mobile').val();
+            if ($.regex.isEmail(email_mobile) || $.regex.isMobile(email_mobile)) {
+                this._make_rss_activate(email_mobile, null, this)
+            } else {
+                $('input.email_mobile').prev('.channel_err').show();
+            }
+        },
+        _before_show_option:function(obj, channel, mail_to){
+            if ($('input.email_mobile').length > 0) {
+                $('input.email_mobile').on('focus', function() {
+                    $('input.email_mobile').prev('.channel_err').hide();
+                })
+            }
+
+            if ($('button.subsc').length > 0) {
+                $('button.subsc').on('click', function() {
+                    $this._fill_email_mobile();
+                })
+            }
+
+            if ($('a.re_mobile').length > 0) {
+                $('a.re_mobile').on('click', function() {
+                    $this._re_generate_email_activate($('a.re_mobile'), channel);
+                })
+            }
+
+            if ($('button.next_f').length > 0) {
+                $('button.next_f').on('click', function() {
+                    $this._mobile_rss_activate();
+                })
+            }
+
+            if ($('input[name="code"]').length > 0) {
+                $('input[name="code"]').on('focus', function() {
+                    $('button.next_f').next('.code_exp').hide();
+                });
+            }
+
+            if ($('a.re_mail').length > 0) {
+                $('a.re_mail').on('click', function() {
+                    var mail = $('span.mail').text();
+                    $this._re_generate_email_activate($('a.re_mail'), mail);
+                })
+            }
+
+            if ($('button.mail_act_now').length > 0) {
+                $('button.mail_act_now').on('click', function() {
+                    var link = $(this).attr('href');
+                    window.location.href = link;
+                })
+
+            }
+
+            if($('button.close_f').length > 0){
+                 $('button.close_f').live('click', function() {
+                    $.fancybox.close();
+                 })   
+            }
+
+            if ($('span.mail').length > 0) {
+                $('span.mail').text(channel)
+                $('button').attr('href', mail_to)
+            }
+
+            if ($('span.mobi').length > 0) {
+                $('span.mobi').text(channel)
+            }
+        },
         _popup: function(obj, channel, mail_to) {
-            $.fancybox.open([{
-                href: obj
-            }], {
+            $.fancybox.open([obj], {
                 beforeShow: function() {
                     $(".fancybox-skin").css({
                         "backgroundColor": "#fff"
                     });
-                    if ($('span.mail').length > 0) {
-                        $('span.mail').text(channel)
-                        $('button').attr('href', mail_to)
-                    }
 
-                    if ($('span.mobi').length > 0) {
-                        $('span.mobi').text(channel)
-                    }
+                    $this._before_show_option(obj, channel, mail_to);
 
                     $('a.re_mail').next('span').remove();
                     $('a.re_mobile').next('span').remove();
 
                 },
                 afterClose: function() {
-                    $('a.rss-btn').html('订阅').removeClass('disabled');
-                    $('input[name="code"]').val('');
-                    $('.code_exp').hide();
+                    $this._destroy();
                 },
                 width: 500,
                 height: 180,
@@ -100,9 +184,6 @@
             });
         },
         _re_generate_email_activate: function(obj, email) {
-            console.log("-----------------------")
-            console.log(obj)
-            console.log("-----------------------")
             $.ajax({
                 type: "POST",
                 url: '/surveys/generate_rss_activate_code',
@@ -130,6 +211,12 @@
                     }
                 }
             });
+        },
+        _destroy: function() {
+            if (this.options.btn) {
+                this.options.btn.html('订阅').removeClass('disabled')
+            }
+            $.Widget.prototype.destroy.call(this);
         }
     })
 })(jQuery)
