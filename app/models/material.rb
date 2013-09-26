534ed5db72189033e604924f6987221e3c062896
Karma <boris.isolet@gmail.com>
2013-09-26 09:26:40 +0800

    space

--- app/models/material.rb
+++ app/models/material.rb
@@ -3,67 +3,67 @@ require 'error_enum'
 require 'securerandom'
 
 class Material
-    include Mongoid::Document
-    include Mongoid::Timestamps
-    include FindTool
-    # 1 for image, 2 for video, 4 for audio, 8 for local image, 16 for local video, 32 for local audio
-    field :material_type, :type => Integer
-    field :title, :type => String
-    field :value, :type => String
-    field :picture_url, :type => String
+  include Mongoid::Document
+  include Mongoid::Timestamps
+  include FindTool
+  # 1 for image, 2 for video, 4 for audio, 8 for local image, 16 for local video, 32 for local audio
+  field :material_type, :type => Integer
+  field :title, :type => String
+  field :value, :type => String
+  field :picture_url, :type => String
 
-    belongs_to :gift, :inverse_of => 'photo'
-    belongs_to :prize, :inverse_of => 'photo'
-    belongs_to :lottery, :inverse_of => 'photo'
-    belongs_to :user
-    belongs_to :user,:inverse_of => 'avatar'
+  belongs_to :gift, :inverse_of => 'photo'
+  belongs_to :prize, :inverse_of => 'photo'
+  belongs_to :lottery, :inverse_of => 'photo'
+  belongs_to :user
+  belongs_to :user,:inverse_of => 'avatar'
 
-    before_save :set_picture_url
+  before_save :set_picture_url
 
-    def set_picture_url
-        if material_type == 1
-            picture_url ||= '/assets/images/img.png'
-        end
+  def set_picture_url
+    if material_type == 1
+      picture_url ||= '/assets/images/img.png'
     end
+  end
 
-    # def self.find_by_id(material_id)
-    #   return Material.where(:_id => material_id).first
-    # end
+  # def self.find_by_id(material_id)
+  #   return Material.where(:_id => material_id).first
+  # end
 
-    def self.create_image(image_url)
-        material = Material.new(:material_type => 1, :value => image_url, :picture_url => image_url)
-        material.save
-        return material
+  def self.create_image(image_url)
+    material = Material.new(:material_type => 1, :value => image_url, :picture_url => image_url)
+    material.save
+    return material
+  end
+
+  def self.check_and_create_new(current_user, material)
+    return ErrorEnum::WRONG_MATERIAL_TYPE unless [1, 2, 4, 8, 16, 32].include?(material["material_type"].to_i)
+    material_inst = Material.new(:material_type => material["material_type"].to_i,
+      :value => material["value"],
+      :title => material["title"],
+      :picture_url => material["picture_url"])
+    material_inst.save
+    if !current_user.nil?
+      current_user.materials << material_inst
+      current_user.save
     end
+    return material_inst
+  end
 
-    def self.check_and_create_new(current_user, material)
-        return ErrorEnum::WRONG_MATERIAL_TYPE unless [1, 2, 4, 8, 16, 32].include?(material["material_type"].to_i)
-        material_inst = Material.new(:material_type => material["material_type"].to_i,
-            :value => material["value"],
-            :title => material["title"],
-            :picture_url => material["picture_url"])
-        material_inst.save
-        if !current_user.nil?
-            current_user.materials << material_inst
-            current_user.save
-        end
-        return material_inst
+  def self.find_by_type(material_type)
+    return [] if !(1..63).to_a.include?(material_type)
+    materials = []
+    Material.all.each do |material|
+      materials << material if material.material_type & material_type > 0
     end
+    return materials
+  end
 
-    def self.find_by_type(material_type)
-        return [] if !(1..63).to_a.include?(material_type)
-        materials = []
-        Material.all.each do |material|
-            materials << material if material.material_type & material_type > 0
-        end
-        return materials
-    end
-
-    def update_material(material)
-      self.update_attributes(:material_type => material["material_type"],
-            :value => material["value"],
-            :title => material["title"],
-            :picture_url => material["picture_url"])
-      return self.save
-    end
+  def update_material(material)
+    self.update_attributes(:material_type => material["material_type"],
+      :value => material["value"],
+      :title => material["title"],
+      :picture_url => material["picture_url"])
+    return self.save
+  end
 end
--- app/models/survey_invitation_history.rb
+++ app/models/survey_invitation_history.rb
@@ -1,16 +1,16 @@
 class SurveyInvitationHistory
-    include Mongoid::Document
-    include Mongoid::Timestamps
-    field :type, :type => String
-    # 0 for ongoing, 1 for finished
-    belongs_to :user
-    belongs_to :survey, index: true
-    index({ survey_id: 1 }, { background: true } )
+  include Mongoid::Document
+  include Mongoid::Timestamps
+  field :type, :type => String
+  # 0 for ongoing, 1 for finished
+  belongs_to :user
+  belongs_to :survey, index: true
+  index({ survey_id: 1 }, { background: true } )
 
-    def self.get_user_ids_sent(survey_id)
-        survey = Survey.find_by_id(survey_id)
-        user_ids_sent = []
-        selected_user_ids = survey.survey_invitation_histories.map { |e| e.user_id.to_s }
-        return selected_user_ids
-    end
+  def self.get_user_ids_sent(survey_id)
+    survey = Survey.find_by_id(survey_id)
+    user_ids_sent = []
+    selected_user_ids = survey.survey_invitation_histories.map { |e| e.user_id.to_s }
+    return selected_user_ids
+  end
 end
