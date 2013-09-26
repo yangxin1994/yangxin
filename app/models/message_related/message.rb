0480a85919e8ebfd85464ead8438ca4ff09b1ecb
Karma <boris.isolet@gmail.com>
2013-09-26 09:23:17 +0800

    space

--- app/models/message_related/message.rb
+++ app/models/message_related/message.rb
@@ -2,33 +2,33 @@
 require "error_enum"
 class Message
 
-    include Mongoid::Document
-    include Mongoid::Timestamps
-    include Mongoid::ValidationsExt
-    extend Mongoid::FindHelper
-    include FindTool
-    field :title, :type => String
-    field :content, :type => String
-    # 0 the message is sent to all users
-    # 1 the message is sent to special users
-    field :type, :type => Integer, default: 0
+  include Mongoid::Document
+  include Mongoid::Timestamps
+  include Mongoid::ValidationsExt
+  extend Mongoid::FindHelper
+  include FindTool
+  field :title, :type => String
+  field :content, :type => String
+  # 0 the message is sent to all users
+  # 1 the message is sent to special users
+  field :type, :type => Integer, default: 0
 
-    default_scope order_by(:created_at.desc)
+  default_scope order_by(:created_at.desc)
 
-    belongs_to :sender, :class_name => "User", :inverse_of => :sended_messages
-    has_and_belongs_to_many :receiver, class_name: "User", inverse_of: :messages
+  belongs_to :sender, :class_name => "User", :inverse_of => :sended_messages
+  has_and_belongs_to_many :receiver, class_name: "User", inverse_of: :messages
 
-    validates :title, :presence => true
-    # validates :content, :presence => true
+  validates :title, :presence => true
+  # validates :content, :presence => true
 
-    scope :unread, ->(t){where(:updated_at.gt => t)}
-    scope :readed, ->(t){where(:updated_at.lt => t)}
+  scope :unread, ->(t){where(:updated_at.gt => t)}
+  scope :readed, ->(t){where(:updated_at.lt => t)}
 
-    index({ updated_at: 1 }, { background: true } )
+  index({ updated_at: 1 }, { background: true } )
 
-    # def self.find_by_id(message_id)
-    #   return self.where(:_id => message_id).first
-    # end
+  # def self.find_by_id(message_id)
+  #   return self.where(:_id => message_id).first
+  # end
 
 
 end
