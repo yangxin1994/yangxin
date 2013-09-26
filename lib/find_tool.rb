0847a6cbdaf9ed5564991c3a7ff6d01274a509d5
Karma <boris.isolet@gmail.com>
2013-09-26 16:43:16 +0800

    merge

--- lib/find_tool.rb
+++ lib/find_tool.rb
@@ -1,45 +1,45 @@
 module FindTool
   def self.included(base)
-	base.extend(MongoidFinder)
+		base.extend(MongoidFinder)
   end
 
   module MongoidFinder
 
-	def find_by_id(id, &block)
-		retval = self.where(:id => id).first
-		if retval.present? && block_given?
-		  retval = yield(retval)
+		def find_by_id(id, &block)
+			retval = self.where(:id => id).first
+			if retval.present? && block_given?
+			  retval = yield(retval)
+			end
+			retval
 		end
-	end
 
-	def find_by_name(name)
-		retval = where(:name => name).first
-	end
+		def find_by_name(name)
+			retval = where(:name => name).first
+		end
 
-	def find_by_email_or_mobile(email_mobile)
-		retval = any_of({:email => email_mobile},{:mobile => email_mobile}).first
-	end  
+		def find_by_email_or_mobile(email_mobile)
+			retval = any_of({:email => email_mobile},{:mobile => email_mobile}).first
+		end  
 
-	def method_missing(method_sym, *arguments, &block)
-	  if method_sym.to_s =~ /^find_by_(.*)_or_(.*)/
-			any_of({$1.to_sym => arguments.first},{$2.to_sym => arguments.first}).first
-	  elsif method_sym.to_s =~ /^find_by_(.*)_and_(.*)$/
-			where($1.to_sym => arguments.first).and($2.to_sym => arguments.last).first
-	  elsif method_sym.to_s =~ /^find_by_(.*)$/
-			where($1.to_sym => arguments.first).first
-	  else
-		  super
-	  end
-	end
+		def method_missing(method_sym, *arguments, &block)
+		  if method_sym.to_s =~ /^find_by_(.*)_or_(.*)/
+				any_of({$1.to_sym => arguments.first},{$2.to_sym => arguments.first}).first
+		  elsif method_sym.to_s =~ /^find_by_(.*)_and_(.*)$/
+				where($1.to_sym => arguments.first).and($2.to_sym => arguments.last).first
+		  elsif method_sym.to_s =~ /^find_by_(.*)$/
+				where($1.to_sym => arguments.first).first
+		  else
+			  super
+		  end
+		end
 
-	def self.respond_to?(method_sym, include_private = false)
-	  if method_sym.to_s =~ /^find_by_(.*)$/
-			true
-	  else
-			super
-	  end
-	end
-
+		def self.respond_to?(method_sym, include_private = false)
+		  if method_sym.to_s =~ /^find_by_(.*)$/
+				true
+		  else
+				super
+		  end
+		end
 
 	# def deprecate(old_method, new_method)
 	#   define_method(old_method) do |*args, &block|
