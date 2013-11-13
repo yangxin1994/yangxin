4d20983ce451fb61fe239491d81c37bdfdbd8e1b
JesseYang <jesse.yang1985@gmail.com>
2013-11-13 09:25:04 +0800

    edit sample attribute bug

--- app/controllers/admin/samples_controller.rb
+++ app/controllers/admin/samples_controller.rb
@@ -239,9 +239,9 @@ class Admin::SamplesController < Admin::AdminController
       when 0 then "-1-1"
       else ""
       end
-      set_attr[:analyze_requirement][:segmentation] = attr[:value_3].split(' ').map { |e| Time.parse("#{e}#{strf}").to_i }
+      attr[:analyze_requirement][:segmentation] = attr[:value_3].split(' ').map { |e| Time.parse("#{e}#{strf}").to_i }
     when DataType::NUMBER_RANGE
-      set_attr[:analyze_requirement][:segmentation] = attr[:value_4].split(' ').map { |e| e.to_f }
+      attr[:analyze_requirement][:segmentation] = attr[:value_4].split(' ').map { |e| e.to_f }
     when DataType::DATE_RANGE
       attr[:date_type] = attr[:date_type_5].to_i
       strf = case attr[:date_type]
