e06f032b04a399e99eba242cf9a0b5903ad8106b
JesseYang <jesse.yang1985@gmail.com>
2013-11-21 16:16:15 +0800

    add field

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -47,6 +47,7 @@ class Answer
   #channel: 1（调研社区）2（邮件订阅），4（短信订阅），8（浏览器插件推送），16（微博发布），32（Folwy发布），64（线上代理发布)
   field :channel, :type => Integer
   field :ip_address, :type => String, default: ""
+  field :remote_ip, :type => String, default: ""
   field :http_user_agent, :type => String, default: ""
   field :is_scanned, :type => Boolean, default: false
   field :is_preview, :type => Boolean, default: false
