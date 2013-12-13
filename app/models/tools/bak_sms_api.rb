04f38390b949422d87594101998c03225901958e
JesseYang <jesse.yang1985@gmail.com>
2013-12-13 14:28:18 +0800

    sms api integration

--- app/models/tools/bak_sms_api.rb
+++ app/models/tools/bak_sms_api.rb
@@ -2,12 +2,12 @@
 require 'rubygems'
 require 'httparty'
 
-class SmsApi # 短信接口
+class BakSmsApi # 短信接口
 
   attr_reader :phone_number, :message
 
   include HTTParty
-  base_uri "http://smsapi.c123.cn"
+  base_uri 'http://sdkhttp.eucp.b2m.cn'
   format  :xml
 
   def initialize(phone_number, message)
@@ -15,18 +15,100 @@ class SmsApi # 短信接口
     @message = message
   end
 
-  AC = "1001@500787040001"
-  AUTHKEY = '14C1984315390F6E2E4054A13AA6E3F1'
-  CGID = 52
+  # test account
+  # CDKEY = "0SDK-EBB-0130-NEXUR"
+  # PASSWORD = '007266'
+  # real account
+  CDKEY = "3SDK-EMY-0130-PIWST"
+  # PASSWORD = '921256'
+  PASSWORD = 'od@2013'
+  CODE = 4699
+  AUTOGRAPH = '［问卷吧］'
+  ##### 注意: 不能使用短信接口发送个人信息(如"老地方见","你在哪"之类)                   #####
+  ##### 否则短信平台会封掉接口，测试时只写两个字"内部测试"加其他必要的程序信息(如校验码)#####
+  ##### 注意!注意!注意!注意!注意!注意!注意!注意!注意!注意!注意!注意!注意!注意注意!!注意!#####
 
-
-  def self.send_sms(phone, message)
-    query_p = {action: "sendOnce", ac: AC, authkey: AUTHKEY, cgid: CGID, c: message, m: phone}
-    result = get('/OpenPlatform/OpenApi',
-        :query => query_p)
-    binding.pry
-    puts result.inspect
+  #序列号注册  web容器第一次启动的时候需要激活该序列号
+  def self.regist_serival_number
+    result = get('/sdkproxy/regist.action', :query => {:cdkey => CDKEY,:password => PASSWORD })
+    puts result.parsed_response
   end
+
+  #注册企业信息  web容器第一次启动的时候，需要注册本企业的相关信息
+  def self.regist_company_info
+    result = get('/sdkproxy/registdetailinfo.action',
+            :query => { :cdkey  => CDKEY,
+                        :password => PASSWORD,
+                        :ename    => '优数调研',
+                        :linkman  => '杨泽曦',
+                        :phonenum => '13488881477',
+                        :mobile   => '13488881477',
+                        :email    => 'yangzexi@oopsdata.com',
+                        :fax      => '010-62800785',
+                        :address  => '北京市海淀区五道口',
+                        :postcode => '100083' })
+
+    puts result.parsed_response
+  end
+
+  def self.get_sent_status
+    result = get('/sdkproxy/getreport.action',
+            :query => { :cdkey => SmsApi::CDKEY,
+                         :password => SmsApi::PASSWORD
+                      })     
+  end
+
+  def self.reset_password
+    result = get('/sdkproxy/changepassword.action', :query => {:cdkey => CDKEY,:password => 'od@2013',:newPassword => PASSWORD})
+  end
+
+  #同步发送即时短信
+  def self.send_sms(type,phone, message)
+    Rails.logger.info "AAAAAAAAAAAAAA"
+    Rails.logger.info phone
+    Rails.logger.info message
+    Rails.logger.info "AAAAAAAAAAAAAA"
+    puts "AAAAAAAAAAAAAA"
+    puts phone
+    puts message
+    puts "AAAAAAAAAAAAAA"
+    return if Rails.env != "production"
+    seqid = Random.rand(10000..9999999999999999).to_i
+
+    result = get('/sdkproxy/sendsms.action',
+            :query => { :cdkey => SmsApi::CDKEY,
+                         :password => SmsApi::PASSWORD,
+                         :phone    => phone,
+                         :message  => message,
+                         :seqid => seqid
+                      }) 
+    #status = get_sent_status
+    SmsHistory.create(mobile:phone,type:type,seqid:seqid)
+    return result
+  end
+
+  #查询短信剩余条数
+  def self.get_remainder
+    result = get('/sdkproxy/querybalance.action',
+        :query => {:cdkey    => SmsApi::CDKEY,
+        :password => SmsApi::PASSWORD  
+        })
+    puts result.parsed_response
+    remainder = result.parsed_response['response']['message'].to_f * 10 
+    return remainder
+  end
+
+  #发送定时短信
+  #sendtime format : yyyymmddhhnnss
+  def self.send_time_sms(phone,message, sendtime)
+    get('/sdkproxy/sendtimesms.action',
+            :query => { :cdkey => SmsApi::CDKEY,
+                        :password => SmsApi::PASSWORD,
+                        :phone    => phone,
+                        :message  => message + AUTOGRAPH,
+                        :sendtime => sendtime})
+  end
+
 
   ################### different types of sms ########################
 
