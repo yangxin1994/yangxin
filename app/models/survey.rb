4c9863692f4ca32badf8b741420fa4763b8f1a93
Davis Gao <naitnix@126.com>
2014-04-21 16:42:55 +0800

    modified for changing express link from static to dynamic

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -686,19 +686,12 @@ class Survey
     csv_string = CSV.generate(:headers => true) do |csv|
       csv << excel_header
       answers.each_with_index do |answer, index|
-<<<<<<< HEAD
-        if answer.finished_at
-          answer_time = Time.at(answer.finished_at) - answer.created_at 
-          answer_time = (answer_time.ceil / 60.0).ceil
-        else
-          answer_time = 0
-=======
         if answer.finished_at.present?
           answer_time = Time.at(answer.finished_at) - answer.created_at
           answer_time = (answer_time.ceil / 60.0).ceil
         else
           answer_time = 0      
->>>>>>> master
+
         end
         line_answer = [answer._id, answer.agent_task.present?.to_s, answer.user.try(:email), answer.user.try(:mobile), answer.remote_ip, "#{answer_time} 分"]
         begin
