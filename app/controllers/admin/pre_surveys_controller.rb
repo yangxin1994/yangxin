3f9c07ccbd69c605e49310121660b5a2e4edbb8d
JesseYang <jesse.yang1985@gmail.com>
2014-05-21 11:04:15 +0800

    controller

--- app/controllers/admin/pre_surveys_controller.rb
+++ app/controllers/admin/pre_surveys_controller.rb
@@ -3,9 +3,25 @@ class Admin::PreSurveysController < Admin::AdminController
 
   layout "layouts/admin-todc"
 
-	# *****************************
-
   def index
-    render text: "presurvey#index, survey id: #{params[:survey_id]}"
+    survey = Survey.find(params[:survey_id])
+    @pre_surveys = survey.pre_surveys
+  end
+
+  def create
+    survey = Survey.find(params[:survey_id])
+    @pre_survey = Presurvey.create(params[:pre_survey])
+    @presurvey.survey = survey
+  end
+
+  def destroy
+    pre_survey = PreSurvey.find(params[:id])
+    pre_survey.destroy
+    redirect_to action: :index
+  end
+
+  def update
+    pre_survey = PreSurvey.find(params[:id])
+    pre_survey.update(params[:pre_survey])
   end
 end
