85ed58fbc44f241fa3361f6efaf8a77caed8eac2
JesseYang <jesse.yang1985@gmail.com>
2014-05-22 15:53:08 +0800

    send invitation emails

--- app/workers/pre_survey_worker.rb
+++ app/workers/pre_survey_worker.rb
@@ -2,9 +2,9 @@ class PreSurveyWorker
   include Sidekiq::Worker
   sidekiq_options :retry => false, :queue => "oopsdata_#{Rails.env}".to_sym
   def perform
-    email_to_send = []
-    mobile_to_send = []
     PreSurvey.where(status: OPEN).each do |pre_survey|
+      email_to_send = []
+      mobile_to_send = []
       publish_survey = Survey.find(pre_survey.publish["survey_id"])
       publish_answers = publish_survey.answers.where(:finished_at.gt pre_survey.last_scan_time)
       selected_answers = publish_answers.select do |a|
@@ -41,8 +41,16 @@ class PreSurveyWorker
           mobile_to_send << email_mobile if pre_survey.publish["type"] == PreSurvey::MOBILE
         end
       end
+      # send emails and messages
+      email_to_send = email_to_send - SurveyInvitationHistory.get_emails_sent(pre_survey.survey_id.to_s)
+      mobile_to_send = mobile_to_send - SurveyInvitationHistory.get_mobiles_sent(pre_survey.survey_id.to_s)
+      MailgunApi.batch_send_survey_email(pre_survey.survey_id.to_s, email_to_send, "email")
+
+      SmsApi.invitation_sms(s_id, sample_id, "")
+
+      email_to_send = []
+      mobile_to_send = []
     end
 
-    # send emails and messages
   end
 end
