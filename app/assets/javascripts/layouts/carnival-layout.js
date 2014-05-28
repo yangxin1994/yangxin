3ee65e07458771d0911aa89dc3492d1eccd11545
Davis Gao <naitnix@126.com>
2014-05-28 10:37:07 +0800

    modified

---
+++ app/assets/javascripts/layouts/carnival-layout.js
@@ -0,0 +1,12 @@
+//=require jquery.easings.min
+//=require jquery.fullPage
+
+$(function() {
+    $('.fullPage').fullpage({
+        verticalCentered: false,
+        slidesColor: ['#f2f2f2', '#4BBFC3', '#7BAABE', '#666666'],
+        loopTop: false,
+        loopBottom: false,
+        autoScrolling: true
+    })
+})
