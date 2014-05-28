33ece5906fb1d2f574deb24f15c103edec293548
JesseYang <jesse.yang1985@gmail.com>
2014-05-28 13:22:15 +0800

    create carnival user

--- app/controllers/carnival/campaigns_controller.rb
+++ app/controllers/carnival/campaigns_controller.rb
@@ -1,4 +1,8 @@
 class Carnival::CampaignsController < Carnival::CarnivalController
   def index
+    if @current_carnival_user.blank?
+      @current_carnival_user = CarnivalUser.create_new(params[:introducer_id])
+      cookies[:carnival_user_id] = carnival_user.id.to_s
+    end
   end
 end
