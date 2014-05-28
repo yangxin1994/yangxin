81eb752d8792f59009c951724ebb3d385c110e57
Davis Gao <naitnix@126.com>
2014-05-28 14:26:59 +0800

    modified

--- app/controllers/carnival/campaigns_controller.rb
+++ app/controllers/carnival/campaigns_controller.rb
@@ -1,4 +1,15 @@
 class Carnival::CampaignsController < Carnival::CarnivalController
   def index
+
+    c_user = User.new unless cookie[:current_user_id].present?
+    @current_user_id = cookie[:current_user_id] || c_user.id
+
+    @prev_survey = Survey.last
+    @prev_answer = @prev_survey.answers.where(:user_id => @current_user_id) 
+    
+    # set cookie ticket true or false
+    # set cookie current_step 0 1 2 3
+     
+
   end
 end
