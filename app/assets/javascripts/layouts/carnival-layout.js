4ac2fddab789f23742757a935935018836045d5c
Davis Gao <naitnix@126.com>
2014-05-29 10:34:59 +0800

    modified

--- app/assets/javascripts/layouts/carnival-layout.js
+++ app/assets/javascripts/layouts/carnival-layout.js
@@ -1,13 +1,4 @@
 //=require jquery.easings.min
 //=require jquery.fullPage
 //=require jquery.cookie
-
-$(function() {
-    $('.fullPage').fullpage({
-        verticalCentered: true,
-        slidesColor: ['#f2f2f2', '#4BBFC3', '#7BAABE', '#666666'],
-        loopTop: false,
-        loopBottom: false,
-        autoScrolling: true
-    })
-})
+//=require ui/widgets/od_popup
--- app/controllers/carnival/campaigns_controller.rb
+++ app/controllers/carnival/campaigns_controller.rb
@@ -1,22 +1,42 @@
 class Carnival::CampaignsController < Carnival::CarnivalController
   def index
-
-
-    # c_user = User.new unless cookie[:current_user_id].present?
-    # @current_user_id = cookie[:current_user_id] || c_user.id
-
-    # @prev_survey = Survey.last
-    # @prev_answer = @prev_survey.answers.where(:user_id => @current_user_id) 
-    
-    # set cookie ticket true or false
-    # set cookie current_step 0 1 2 3
-     
-
-
     if @current_carnival_user.blank?
       @current_carnival_user = CarnivalUser.create_new(params[:introducer_id])
       cookies[:carnival_user_id] = @current_carnival_user.id.to_s
     end
 
+    # pre_survey = Carnival::PRE_SURVEY
+
+    # background_survey = Carnival::BACKGROUND_SURVEY
+
+    # surveys = @current_carnival_user.survey_order.each_slice(5).to_a
+
+    # step_arr = @current_carnival_user.survey_status.each_slice(5).to_a
+
+    # step = 0
+
+    # if (step_arr[2].include?(CarnivalUser::UNDER_REVIEW) || step_arr[2].include?(CarnivalUser::UNDER_REVIEW) || step_arr[2].include?(CarnivalUser::FINISH) )
+    #   step = 3
+    # elsif (step_arr[1].include?(CarnivalUser::UNDER_REVIEW) || step_arr[1].include?(CarnivalUser::UNDER_REVIEW) || step_arr[1].include?(CarnivalUser::FINISH) )  
+    #   step = 2
+    # elsif (step_arr[0].include?(CarnivalUser::UNDER_REVIEW) || step_arr[0].include?(CarnivalUser::UNDER_REVIEW) || step_arr[0].include?(CarnivalUser::FINISH) )
+    #   step = 1
+    # end
+
+    # @obj = {
+    #   pre_status:@current_carnival_user.pre_survey_status,
+    #   step:step,
+    #   pre_survey:pre_survey,
+    #   background_survey:background_survey,
+    #   t1_surveys:surveys[0],
+    #   t2_surveys:surveys[1],
+    #   t3_surveys:surveys[2],
+    #   t1_status:step_arr[0],
+    #   t2_status:step_arr[1],
+    #   t3_status:step_arr[2]
+    # }
+
+    return @obj
+       
   end
 end
