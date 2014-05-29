8020c374abf0c771470e861d12977938fd9e26e3
oliverhuang <oliverhdh@gmail.com>
2014-05-29 15:57:58 +0800

    fix bug: disable next page button for survey filler. close #395

--- app/assets/javascripts/quill/views/survey_filler.js
+++ app/assets/javascripts/quill/views/survey_filler.js
@@ -189,7 +189,8 @@ $(function(){
 					var next_btn = $('#next_btn'), prev_btn = $('#prev_btn');
 					// Disable next button for some time (don't disable it for allow_pageup==true survey, or previewing)
 					var has_not_empty_answer = (_.find(answers || [], function(a) { return a != null; }) != null);
-					if(!has_not_empty_answer && !this.options.is_preview && prev_btn.length > 0) {
+					if(!this.options.is_preview && (prev_btn.length == 0 || !has_not_empty_answer)) {
+            console.log('===========')
 						$.util.disable(next_btn);
 						var old_text = next_btn.text();
 						function _update_btn() {
