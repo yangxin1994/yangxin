a705541d30cbfe1805300b91e1cd3f220210e4b2
JesseYang <jesse.yang1985@gmail.com>
2014-05-29 16:37:20 +0800

    all but admin

--- app/models/carnival_related/carnival_order.rb
+++ app/models/carnival_related/carnival_order.rb
@@ -7,13 +7,12 @@ class CarnivalOrder
   include FindTool
 
   # status
-  WAIT = 1
   HANDLE = 2
   SUCCESS = 4
   FAIL = 8
   CANCEL = 16
-  FROZEN = 32
   REJECT = 64
+  UNDER_REVIEW = 128
 
   # esai status
   ESAI_HANDLE = 3
@@ -21,20 +20,19 @@ class CarnivalOrder
   ESAI_FAIL = 5
 
   # type
-  REAL = 2
-  MOBILE_CHARGE = 4
+  # 0代表第二个大任务的抽奖，1代表第三个大任务的抽奖，2代表分享成功的抽奖，3代表第一个大任务的10元充值卡，4代表第三个大任务的10充值卡
+  STAGE_2 = 0
+  STAGE_3_LOTTERY = 1
+  SHARE = 2
+  STAGE_1 = 3
+  STAGE_3 = 4
 
-  field :stage, type: Integer
+
   field :code, type: String, default: ->{ Time.now.strftime("%Y%m%d") + sprintf("%05d",rand(10000)) }
+  field :status, type: Integer
   field :type, :type => Integer
-  field :status, :type => Integer, :default => 1
-  field :amount, :type => Integer, :default => 0
+  field :amount, :type => Integer
   field :mobile, :type => String
-  field :receiver, :type => String
-  field :address, :type => String
-  field :street_info, :type => String
-  field :postcode, :type => String
-  field :express_info, :type => Hash
   field :reviewed_at, :type => Integer
   field :handled_at, :type => Integer
   field :finished_at, :type => Integer
@@ -44,6 +42,7 @@ class CarnivalOrder
   field :esai_status, :type => Integer
 
   belongs_to :carnival_user
+  belongs_to :carnival_prize
 
   index({ code: 1 }, { background: true } )
   index({ status: 1 }, { background: true } )
@@ -51,353 +50,34 @@ class CarnivalOrder
   index({ amount: 1 }, { background: true } )
   index({ type: 1, status: 1, esai_order_id: 1}, { background: true } )
 
-  def self.create_redeem_order(sample_id, gift_id, amount, point, opt = {})
-    gift  = Gift.normal.find_by_id(gift_id)
-    return ErrorEnum::ORDER_ERROR if amount.to_i < 1
-    point = gift.point.to_i  * amount.to_i
-    sample = User.sample.find_by_id(sample_id)
-    return ErrorEnum::POINT_NOT_ENOUGH if sample.point.to_i < point.to_i 
-    return ErrorEnum::SAMPLE_NOT_EXIST if sample.nil?
-    gift = Gift.normal.find_by_id(gift_id)
-    return ErrorEnum::GIFT_NOT_EXIST if gift.nil?
-    order = Order.create(:source => REDEEM_GIFT, :amount => amount, :point => point, :type => gift.type)
-    order.sample = sample
-    order.gift = gift
-    case gift.type
-    when Gift::MOBILE_CHARGE
-      order.mobile = opt["mobile"]
-    when Gift::ALIPAY
-      order.alipay_account = opt["alipay_account"]
-    when Gift::JIFENBAO
-      order.alipay_account = opt["alipay_account"]
-    when Gift::QQ_COIN
-      order.qq = opt["qq"]
-    when Gift::VIRTUAL
-    when Gift::REAL
-      order.receiver = opt["receiver"]
-      order.mobile = opt["mobile"]
-      order.address = opt["address"]
-      order.street_info = opt["street_info"]
-      order.postcode = opt["postcode"]
-    end
-    order.save
-    sample.point -= point 
-    sample.save
-    PointLog.create_redeem_point_log(point,gift_id,sample_id)
-    RedeemLog.create_gift_exchange_logs(amount,point,gift.type,order.id,gift_id,sample_id)
-    order.auto_handle
-    return order
-  end
+  def handle
+    return if self.status != WAIT
+    return if [STAGE_1, STAGE_2, STAGE_3].include?(self.type)
 
-  def self.create_lottery_order(answer_id, sample_id, survey_id, prize_id, ip_address, opt = {})
-    sample = User.sample.find_by_id(sample_id)
-    survey = Survey.find_by_id(survey_id)
-    return ErrorEnum::SURVEY_NOT_EXIST if survey.nil?
-    prize = Prize.normal.find_by_id(prize_id)
-    return ErrorEnum::PRIZE_NOT_EXIST if prize.nil?
-    order = Order.new(:source => WIN_IN_LOTTERY, :type => prize.type, :amount => prize.amount)
-    order.sample = sample if sample.present?
-    order.survey = survey
-    order.prize = prize
-    case prize.type
-    when Prize::MOBILE_CHARGE
-      order.mobile = opt["mobile"]
-    when Prize::ALIPAY
-      order.alipay_account = opt["alipay_account"]
-    when Prize::JIFENBAO
-      order.alipay_account = opt["alipay_account"]
-    when Prize::QQ_COIN
-      order.qq = opt["qq"]
-    when Prize::VIRTUAL
-    when Prize::REAL
-      order.receiver = opt["receiver"]
-      order.address = opt["address"]
-      order.postcode = opt["postcode"]
-      order.mobile = opt["mobile"]
-      order.street_info = opt["street_info"]
-    end
-    order.status = FROZEN if opt["status"] == FROZEN
-    order.save
-    order.auto_handle
-
-    ##synchro  reverver info 
-    if opt['info_sys'].to_s == 'true'
-      option = {}
-      option["receiver"]    = order[:receiver] 
-      option["mobile"]      = order[:mobile]
-      option["address"]     = order[:address]
-      option["street_info"] = order[:street_info]
-      option["postcode"]    = order[:postcode]
-      sample.set_receiver_info(option) if sample.present?
-    end 
-
-    LotteryLog.create_succ_lottery_Log(answer_id:answer_id,
-                      order_id:order.id,
-                      survey_id:survey_id,
-                      sample_id:sample_id,
-                      ip_address:ip_address,
-                      prize_id:prize_id
-                      )
-    return order
-  end
-
-  def self.create_answer_alipay_order(answer, reward)
-    order_info = { "alipay_account" => reward["alipay_account"] }
-    order_info.merge!("status" => FROZEN) if answer.status == Answer::UNDER_REVIEW
-    Order.create_answer_order(
-      answer.id.to_s,
-      answer.user.try(:_id),
-      answer.survey._id.to_s,
-      ALIPAY,
-      reward["amount"],
-      order_info)
-  end
-
-  def self.create_answer_jifenbao_order(answer, reward)
-    order_info = { "alipay_account" => reward["alipay_account"] }
-    order_info.merge!("status" => FROZEN) if answer.status == Answer::UNDER_REVIEW
-    Order.create_answer_order(
-      answer.id.to_s,
-      answer.user.try(:_id),
-      answer.survey._id.to_s,
-      JIFENBAO,
-      reward["amount"],
-      order_info)
-  end
-
-  def self.create_answer_mobile_order(answer, reward)
-    order_info = { "mobile" => reward["mobile"] }
-    order_info.merge!("status" => FROZEN) if answer.status == Answer::UNDER_REVIEW
-    Order.create_answer_order(
-      answer.id.to_s,
-      answer.user.try(:_id),
-      answer.survey._id.to_s,
-      SMALL_MOBILE_CHARGE,
-      reward["amount"],
-      order_info)
-  end
-
-
-  def self.create_answer_order(answer_id, sample_id, survey_id, type, amount, opt = {})
-    sample = User.sample.find_by_id(sample_id)
-    survey = Survey.find_by_id(survey_id)
-    answer = Answer.find(answer_id)
-    return ErrorEnum::SURVEY_NOT_EXIST if survey.nil?
-    order = Order.create(:source => ANSWER_SURVEY, :amount => amount, :type => type)
-    order.sample = sample if sample.present?
-    order.survey = survey
-    order.answer = answer
-    case type
-    when SMALL_MOBILE_CHARGE
-      order.mobile = opt["mobile"]
-    when ALIPAY
-      order.alipay_account = opt["alipay_account"]
-    when JIFENBAO
-      order.alipay_account = opt["alipay_account"]
-    end
-    order.status = opt["status"] if opt["status"].present?
-    order.save
-    order.auto_handle
-    return order
-  end
-
-  def auto_handle
-    return false if self.status != WAIT
-    return false if self.type != MOBILE_CHARGE
-    # retval = EsaiApi.new.charge_phone(self.mobile, self.amount, "None")
+    # only hanlde the mobile orders that are in wait status
     self.status = HANDLE
     self.esai_status = ESAI_HANDLE
     self.handled_at = Time.now
     self.save
-    ChargeWorker.perform_async(self.id.to_s, self.mobile, self.amount)
-=begin
-    if retval.nil?
-      self.esai_status = ESAI_FAIL
-    else
-      self.esai_status = ESAI_HANDLE
-      self.esai_order_id = retval
-    end
-=end
+    CarnivalChargeWorker.perform_async(self.id.to_s, self.mobile, self.amount)
   end
 
-  def self.recharge_fail_mobile
-    Order.where(esai_status: ESAI_FAIL, status: HANDLE, type: MOBILE_CHARGE).each do |order|
-      order.update_attributes(status: WAIT)
-      order.auto_handle
+  def under_review
+    if self.status == REJECT
+      self.update_attributes(status: UNDER_REVIEW)
     end
   end
 
-  def check_result
-    return nil if self.esai_order_id.blank?
-    retval = EsaiApi.new.check_result(self.esai_order_id, "None")
-    case retval
-    when 0, 1, 2
-      self.update_attributes({status: HANDLE, esai_status: ESAI_HANDLE})
-    when 4
-      self.update_attributes({status: SUCCESS, esai_status: ESAI_SUCCESS})
-      self.send_mobile_charge_success_message
-    when 5
-      self.update_attributes({status: HANDLE, esai_status: ESAI_FAIL})
+  def reject
+    if self.status == UNDER_REVIEW
+      self.update_attributes(status: REJECT)
     end
-    return self.esai_status
   end
 
-  def send_mobile_charge_success_message
-    SmsWorker.perform_async("charge_notification", self.mobile, "", gift_name: "#{self.amount}元话费")
-  end
-
-  def manu_handle
-    return ErrorEnum::WRONG_ORDER_STATUS if self.status != WAIT
-    self.status = HANDLE
-    self.handled_at = Time.now.to_i
-    return self.save
-  end
-
-  def finish(success, remark = "")
-    return ErrorEnum::WRONG_ORDER_STATUS if self.status != HANDLE
-    self.status = success ? SUCCESS : FAIL
-    self.remark = remark
-    self.finished_at = Time.now.to_i
-    if self.status == FAIL && self.source == REDEEM_GIFT
-      sample = self.sample
-      sample.point = sample.point + point
-      sample.save
-      PointLog.create_admin_operate_point_log(point, "兑换失败，积分返还", sample._id.to_s)
+  def pass
+    if self.status == UNDER_REVIEW
+      self.update_attributes(status: WAIT)
+      self.hanlde
     end
-    return self.save
-  end
-
-  def self.search_orders(options = {})
-    options[:status] = options[:status].to_i
-    options[:source] = options[:source].to_i
-    options[:type] = options[:type].to_i
-    options.delete(:type) if options[:type] == 0
-    if options[:email].present?
-      orders = User.find_by_email(options[:email]).try(:orders)
-      orders = orders.nil? ? [] : orders.desc(:created_at)
-    elsif options[:mobile].present?
-      orders = User.find_by_mobile(options[:mobile]).try(:orders)
-      orders = orders.nil? ? [] : orders.desc(:created_at)
-    elsif options[:code].present?
-      orders = Order.where(:code => /#{options[:code]}/).desc(:created_at)
-    else
-      orders = Order.all.desc(:created_at)
-    end
-    if options[:status].present? && options[:status] != 0
-      status_ary = Tool.convert_int_to_base_arr(options[:status])
-      orders = orders.where(:status.in => status_ary)
-    end
-    if options[:source].present? && options[:source] != 0
-      source_ary = Tool.convert_int_to_base_arr(options[:source])
-      orders = orders.where(:source.in => source_ary)
-    end
-    if options[:type].present? && options[:type] != 0
-      type_ary = Tool.convert_int_to_base_arr(options[:type])
-      orders = orders.where(:type.in => type_ary)
-    end 
-    if options[:date_max].present?
-      orders = orders.where(:created_at.lt => Time.parse(options[:date_max]))
-    elsif options[:date_min].present?
-      orders = orders.where(:created_at.gt => Time.parse(options[:date_min]))
-    else
-      if options[:date].present?
-        _qdate = Time.now - options[:date].to_i.days
-        orders = orders.where(:created_at.gt => _qdate)
-      end
-    end
-    return orders      
-  end
-
-
-  def update_express_info(express_info)
-    self.express_info = express_info
-    return self.save
-  end
-
-  def update_remark(remark)
-    self.remark = remark
-    return self.save
-  end
-
-  def info_for_admin
-    if self.source == REDEEM_GIFT
-      self.write_attribute(:gift_name, self.gift.try(:title))
-    elsif self.source == WIN_IN_LOTTERY
-      self.write_attribute(:prize_name, self.prize.try(:title))
-    end
-    self.write_attribute(:gift_id, (self.gift || self.prize).try(:_id))
-    self.write_attribute(:user_email_mobile, self.sample.try(:email) || self.sample.try(:mobile))
-    self.write_attribute(:user_id, self.sample.try(:id))
-    if self.type == 2
-      self.write_attribute(:address_str, address_str)
-    end
-    return self
-  end
-
-  def update_status(handle = true)
-    self.update_attributes({"status" => Order::WAIT, "reviewed_at" => Time.now.to_i}) if self.answer.is_finish
-    self.update_attributes({"status" => Order::REJECT, "reviewed_at" => Time.now.to_i} ) if self.answer.is_reject
-    self.auto_handle if handle
-  end
-
-  def info_for_sample
-    order_obj = {}
-    order_obj["_id"] = self._id.to_s
-    order_obj["created_at"] = self.created_at.to_i
-    order_obj["status"] = self.status
-    order_obj["source"] = self.source
-    order_obj["amount"] = self.amount
-    order_obj["type"] = self.type
-    if self.source == REDEEM_GIFT
-      order_obj["point"] = self.point
-      order_obj["title"] = self.gift.try(:title)
-      order_obj["picture_url"] = self.gift.try(:photo).try(:picture_url)
-      order_obj["gift_id"] = self.gift.try(:_id)
-    elsif self.source == WIN_IN_LOTTERY
-      order_obj["title"] = self.prize.try(:title)
-      order_obj["picture_url"] = self.prize.try(:photo).try(:picture_url)
-    end
-    return order_obj
-  end
-
-
-  def info_for_sample_detail
-    order_obj = JSON.parse(self.to_json)
-    order_obj["created_at"] = self.created_at.to_i
-    order_obj["survey_title"] = self.survey.title if !self.survey.nil?
-    order_obj["survey_id"] = self.survey._id.to_s if !self.survey.nil?
-    return order_obj
-  end
-
-  def address_str
-    QuillCommon::AddressUtility.find_province_city_town_by_code(self.address) + " " +
-      self.street_info.to_s + " " + self.postcode.to_s + " " + self.receiver.to_s    
-  end
-
-  def self.to_excel
-    self.criteria.map do |order|
-      item = order.gift || order.prize
-      sample = order.sample
-      unless sample && item
-        next
-      end
-      case order.type
-      when 1
-        "#{sample.email || sample.mobile},#{item.title}-#{order.amount},支付宝账号:#{order.alipay_account}"
-      when 2
-        "#{sample.email || sample.mobile},#{item.title}-#{order.amount},地址:#{order.address_str} 邮编:#{order.postcode} 收件人:#{order.receiver} 电话:#{order.mobile}"
-      when 4
-        "#{sample.email || sample.mobile},#{item.title}-#{order.amount},电话:#{order.mobile || order.sample.mobile}"
-      when 8
-        "#{sample.email || sample.mobile},#{item.title}-#{order.amount},支付宝账号:#{order.alipay_account}"
-      when 16
-        "#{sample.email || sample.mobile},#{item.title}-#{order.amount},集分宝账号:#{order.alipay_account}"
-      when 32
-        "#{sample.email || sample.mobile},#{item.title}-#{order.amount},QQ:#{order.qq}"
-      when 64
-        "#{sample.email || sample.mobile},#{item.title}-#{order.amount},电话:#{order.mobile || order.sample.mobile}"
-      end
-    end.join("\n")
   end
 end
-
--- app/models/carnival_related/carnival_user.rb
+++ app/models/carnival_related/carnival_user.rb
@@ -6,21 +6,31 @@ class CarnivalUser
   include Mongoid::Timestamps
   include FindTool
 
+  # survey status
   NOT_EXIST = 0
-  EDIT = 1
   REJECT = 2
   UNDER_REVIEW = 4
   FINISH = 32
 
-  field :email, type: String, default: ""
-  field :introducer_id, type: String
+  # reward_status
+  NOT_EXIST = 0
+  EXIST = 1
 
-  # 0 for not exist, 1 for edit, 2 for reject, 32 for finish
+
+  field :email, type: String, default: ""
+  field :mobile, type: String, default: ""
+  field :introducer_id, type: String
+  field :introducer_reward_assigned, type: Boolean, default: false
+
+  # 0 for not exist, 2 for reject, 32 for finish
   field :pre_survey_status, type: Integer, default: 0
   field :background_survey_status, type: Integer, default: 0
   field :survey_order, type: Array
   field :survey_status, type: Array, default: Array.new(14) { 0 }
-  field :reward_status, type: Array, default: Array.new(3) { 0 }
+  field :lottery_status, type: Array, default: Array.new(2) { 0 }
+
+  field :share_num, type: Integer, default: 0
+  field :share_lottery_num, type: Integer, default: 0
 
   has_many :answers
   has_many :carnival_orders
@@ -78,12 +88,44 @@ class CarnivalUser
     index = self.survey_order.index(answer.survey_id.to_s)
     self.survey_status[index] = UNDER_REVIEW
     self.save
+
+    # handle order
+    if index <= 4
+      order = self.orders.where(type: CarnivalOrder::STAGE_1).first
+      if !(self.survey_status[0..4] & [REJECT, NOT_EXIST, FINISH]).present?
+        order.under_review if order.present?
+      end
+    elsif index <= 9
+      order = self.orders.where(type: CarnivalOrder::STAGE_2).first
+      if !(self.survey_status[5..9] & [REJECT, NOT_EXIST, FINISH]).present?
+        order.under_review if order.present?
+      end
+    else
+      o1 = self.orders.where(type: CarnivalOrder::STAGE_3).first
+      o2 = self.orders.where(type: CarnivalOrder::STAGE_3_LOTTERY).first
+      if (self.survey_status[10..13] & [REJECT, NOT_EXIST, FINISH]).present?
+        o1.under_review if o1.present?
+        o2.under_review if o2.present?
+      end
+    end
   end
 
   def survey_reviewed(answer_id, answer_status)
     answer = Answer.find(answer_id)
     index = self.survey_order.index(answer.survey_id.to_s)
     self.survey_status[index] = answer_status == Answer::FINISH ? FINISH : REJECT
+
+    if answer_status == Answer::FINISH
+      self.survey_status[index] = FINISH
+    else
+      self.survey_status[index] = REJECT
+      if answer.repeat_time == 0
+        answer.set_redo
+        answer.repeat_time += 1
+        answer.save
+      end
+    end
+
     self.save
 
     # update quota if the answer passed review
@@ -93,7 +135,40 @@ class CarnivalUser
     end
 
     # handle order
+    if index <= 4
+      order = self.orders.where(type: CarnivalOrder::STAGE_1).first
+      if (self.survey_status[0..4] & [REJECT]).present?
+        order.reject if order.present?
+      elsif (self.survey_status[0..4] & [REJECT, NOT_EXIST, UNDER_REVIEW]).present?
+        order.pass if order.present?
+      end
+    elsif index <= 9
+      order = self.orders.where(type: CarnivalOrder::STAGE_2).first
+      if (self.survey_status[5..9] & [REJECT]).present?
+        order.reject if order.present?
+      elsif (self.survey_status[5..9] & [REJECT, NOT_EXIST, UNDER_REVIEW]).present?
+        order.pass if order.present?
+      end
+    else
+      o1 = self.orders.where(type: CarnivalOrder::STAGE_3).first
+      o2 = self.orders.where(type: CarnivalOrder::STAGE_3_LOTTERY).first
+      if (self.survey_status[10..13] & [REJECT]).present?
+        o1.reject if o1.present?
+        o2.reject if o2.present?
+      elsif (self.survey_status[10..13] & [REJECT, NOT_EXIST, UNDER_REVIEW]).present?
+        o1.pass if o1.present?
+        o2.pass if o2.present?
+      end
+    end
 
+    # give introducer lottery chance
+    if (self.survey_status & [NOT_EXIST, REJECT, UNDER_REVIEW]).blank? && self.introducer_id.present? && !self.introducer_reward_assigned
+      introducer = CarnivalUser.find(self.introducer_id)
+      introducer.share_num += 1
+      introducer.save
+      self.introducer_reward_assigned = true
+      self.save
+    end
   end
 
   def fill_answer(answer)
@@ -107,28 +182,176 @@ class CarnivalUser
       qid = "538591d6eb0e5b7282000007"
       if !pre_survey_answer.answer_content[qid]["selection"].include?(613766637056106)
         # hide A1-A7
+        ["53868990eb0e5ba25700002a", "53868990eb0e5ba25700002b", "53868990eb0e5ba25700002c", "53868990eb0e5ba25700002d", "53868990eb0e5ba25700002e", "53868990eb0e5ba25700002f", "53868990eb0e5ba257000030"].each do |qid|
+          answer.answer_content[qid] = {}
+        end
       end
       # 3. 预调研里过去一年在“笔记本电脑/台式机”看过电影的用户才回答B1-B6。
       if !pre_survey_answer.answer_content[qid]["selection"].include?(9419552569209770)
         # hide B1-B6
+        ["53868990eb0e5ba257000031", "53868990eb0e5ba257000032", "53868990eb0e5ba257000033", "53868990eb0e5ba257000034", "53868990eb0e5ba257000035", "53868990eb0e5ba257000036"].each do |qid|
+          answer.answer_content[qid] = {}
+        end
       end
       # 4. 预调研里过去一年在“平板”或“手机”看过电影的用户才回答C1-C6。
       if (pre_survey_answer.answer_content[qid]["selection"] & [2214434698414035, 22230203902982316]).blank?
         # hide C1-C6
+        ["53868990eb0e5ba257000037", "53868990eb0e5ba257000038", "53868990eb0e5ba257000039", "53868990eb0e5ba25700003a", "53868990eb0e5ba25700003b", "53868990eb0e5ba25700003c"].each do |qid|
+          answer.answer_content[qid] = {}
+        end
       end
       # 5. 预调研里过去一年在“电视”看过电影的用户才回答D1-D6。
       if !pre_survey_answer.answer_content[qid]["selection"].include?(11346015633717332)
         # hide D1-D6
+        ["53868990eb0e5ba25700003d", "53868990eb0e5ba25700003e", "53868990eb0e5ba25700003f", "53868990eb0e5ba257000040", "53868990eb0e5ba257000041", "53868990eb0e5ba257000042"].each do |qid|
+          answer.answer_content[qid] = {}
+        end
       end
     end
     
-    # 538436c9eb0e5bf8cf00003d
-    # 1. 所有样本都回答E1-E6题。
-    # 2. 预调研里过去一年在“电视”看过电影的用户才回答D1-D16题。
+    s2_id = "538436c9eb0e5bf8cf00003d"
+    if answer.survey_id.to_s == s2_id
+      # 1. 所有样本都回答E1-E6题。
+      # 2. 预调研里过去一年在“电视”看过电影的用户才回答D1-D16题。
+      qid = "538591d6eb0e5b7282000007"
+      if !pre_survey_answer.answer_content[qid]["selection"].include?(11346015633717332)
+        # hide D1-D16
+        ["538436c9eb0e5bf8cf000052", "538436c9eb0e5bf8cf000053", "538436c9eb0e5bf8cf000054", "538436c9eb0e5bf8cf000055", "538436c9eb0e5bf8cf000056", "538436c9eb0e5bf8cf000057", "538436c9eb0e5bf8cf000058", "538436c9eb0e5bf8cf000059", "538436c9eb0e5bf8cf00005a", "538436c9eb0e5bf8cf00005b", "538436c9eb0e5bf8cf00005c", "538436c9eb0e5bf8cf00005d", "538436c9eb0e5bf8cf00005e", "538436c9eb0e5bf8cf00005f", "538436c9eb0e5bf8cf000060", "538436c9eb0e5bf8cf000061"].each do |qid|
+          answer.answer_content[qid] = {}
+        end
+      end
+    end
 
-    # 5384282deb0e5bbcb900002b
-    # “男性”回答B1-B9，“女性”回答B10-B27
+    s3_id = "5384282deb0e5bbcb900002b"
+    if answer.survey_id.to_s == s3_id
+      # “男性”回答B1-B9，“女性”回答B10-B27
+      qid = "538591f8eb0e5b7282000009"
+      if pre_survey_answer.answer_content[qid]["selection"].include?(9735976263679518)
+        # male, hide B10-B27
+        ["5384282deb0e5bbcb9000041", "5384282deb0e5bbcb9000042", "5384282deb0e5bbcb9000043", "5384282deb0e5bbcb9000044", "5384282deb0e5bbcb9000045", "5384282deb0e5bbcb9000046", "5384282deb0e5bbcb9000047", "5384282deb0e5bbcb9000048", "5384282deb0e5bbcb9000049", "5384282deb0e5bbcb900004a", "5384282deb0e5bbcb900004b", "5384282deb0e5bbcb900004c", "5384282deb0e5bbcb900004d", "5384282deb0e5bbcb900004e", "5384282deb0e5bbcb900004f", "5384282deb0e5bbcb9000050", "5384282deb0e5bbcb9000051", "5384282deb0e5bbcb9000052", "5384282deb0e5bbcb9000053", "5384282deb0e5bbcb9000054", "5384282deb0e5bbcb9000055", "5384282deb0e5bbcb9000056", "5384282deb0e5bbcb9000057", "5384282deb0e5bbcb9000058", "5384282deb0e5bbcb9000059", "5384282deb0e5bbcb900005a", "5384282deb0e5bbcb900005b", "5384282deb0e5bbcb900005c", "5384282deb0e5bbcb900005d", "5384282deb0e5bbcb900005e", "5384282deb0e5bbcb900005f", "5384282deb0e5bbcb9000060", "5384282deb0e5bbcb9000061", "5384282deb0e5bbcb9000062", "5384282deb0e5bbcb9000063", "5384282deb0e5bbcb9000064", "5384282deb0e5bbcb9000065", "5384282deb0e5bbcb9000066", "5384282deb0e5bbcb9000067", "5384282deb0e5bbcb9000068", "5384282deb0e5bbcb9000069"].each do |qid|
+          answer.answer_content[qid] = {}
+        end
+      else
+        # femail, hide B1-B9
+        ["5384282deb0e5bbcb900002e", "5384282deb0e5bbcb900002f", "5384282deb0e5bbcb9000030", "5384282deb0e5bbcb9000031", "5384282deb0e5bbcb9000032", "5384282deb0e5bbcb9000033", "5384282deb0e5bbcb9000034", "5384282deb0e5bbcb9000035", "5384282deb0e5bbcb9000036", "5384282deb0e5bbcb9000037", "5384282deb0e5bbcb9000038", "5384282deb0e5bbcb9000039", "5384282deb0e5bbcb900003a", "5384282deb0e5bbcb900003b", "5384282deb0e5bbcb900003c", "5384282deb0e5bbcb900003d", "5384282deb0e5bbcb900003e", "5384282deb0e5bbcb900003f", "5384282deb0e5bbcb9000040"].each do |qid|
+          answer.answer_content[qid] = {}
+        end
+      end
+    end
 
     answer.save
+  end
+
+  def draw_second_stage_lottery(amount, mobile)
+    if (self.survey_status[5..9] & [NOT_EXIST, REJECT]).present?
+      return -2
+    end
+    if self.lottery_status[0] == EXIST
+      return -3
+    end
+    if amount == 20
+      p = 0.5
+    else
+      p = 0.2
+    end
+    return -4 if rand < p
+    self.lottery_status[0] = EXIST
+    self.save
+    # create order
+    order = CarnivalOrder.create(type: CarnivalOrder::STAGE_2, mobile: mobile, amount: amount)
+    order.carnival_user = self
+    if self.survey_status[5..9].include?(UNDER_REVIEW)
+      order.status = CarnivalOrder::UNDER_REVIEW
+    else
+      order.status = CarnivalOrder::WAIT
+    end
+    order.save
+    order.handle
+    return order.id
+  end
+
+  def create_first_stage_order(mobile)
+    if (self.survey_status[0..4] & [NOT_EXIST, REJECT]).present?
+      return -2
+    end
+    if self.orders.where(type: CarnivalOrder::STAGE_1).present?
+      return -3
+    end
+    # create order
+    order = CarnivalOrder.create(type: CarnivalOrder::STAGE_1, mobile: mobile, amount: 10)
+    order.carnival_user = self
+    if self.survey_status[0..4].include?(UNDER_REVIEW)
+      order.status = CarnivalOrder::UNDER_REVIEW
+    else
+      order.status = CarnivalOrder::WAIT
+    end
+    order.save
+    order.handle
+    return order.id
+  end
+
+  def create_third_stage_mobile_order(mobile)
+    if (self.survey_status[10..13] & [NOT_EXIST, REJECT]).present?
+      return -2
+    end
+    if self.orders.where(type: CarnivalOrder::STAGE_3).present?
+      return -3
+    end
+    # create order
+    order = CarnivalOrder.create(type: CarnivalOrder::STAGE_3, mobile: mobile, amount: 10)
+    order.carnival_user = self
+    if self.survey_status[10..13].include?(UNDER_REVIEW)
+      order.status = CarnivalOrder::UNDER_REVIEW
+    else
+      order.status = CarnivalOrder::WAIT
+    end
+    order.save
+    order.handle
+    return order.id
+  end
+
+  def draw_third_stage_lottery(mobile)
+    if (self.survey_status[10..13] & [NOT_EXIST, REJECT]).present?
+      return -2
+    end
+    if self.lottery_status[1] == EXIST
+      return -3
+    end
+    self.lottery_status[1] == EXIST
+    self.save
+
+    ### draw
+    prize = CarnivalPrize.draw
+
+    return -4 if prize.blank?
+    order = CarnivalOrder.create(type: CarnivalOrder::STAGE_3_LOTTERY, mobile: mobile)
+    order.prize = prize
+    order.carnival_user = self
+    if self.survey_status[10..13].include?(UNDER_REVIEW)
+      order.status = CarnivalOrder::UNDER_REVIEW
+    else
+      order.status = CarnivalOrder::WAIT
+    end
+    order.save
+    return order.id
+  end
+
+  def draw_share_lottery(mobile)
+    if self.share_num <= self.share_lottery_num
+      return -3
+    end
+    self.share_lottery_num += 1
+    self.save
+
+    ### draw
+    prize = CarnivalPrize.draw
+
+    return -4 if prize.blank?
+    order = CarnivalOrder.create(type: CarnivalOrder::SHARE, mobile: mobile)
+    order.prize = prize
+    order.carnival_user = self
+    order.status = CarnivalOrder::WAIT
+    order.save
+    return order.id
   end
 end
