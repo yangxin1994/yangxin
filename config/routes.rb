0f7837104eb7576869aa158117a3cbfa1043fddc
JesseYang <jesse.yang1985@gmail.com>
2014-06-10 17:28:35 +0800

    sidekiq

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 require 'sidekiq/web'
 OopsData::Application.routes.draw do
 
+  # mount Sidekiq::Web, at: "/sidekiq"
+
   # match '*path' => 'welcome#index'
   # get '/:unique_key' => 'utility/short_urls#show', :constraints => { :unique_key => /~.+/ }
   match '/:unique_key' => 'mongoid_shortener/shortened_urls#translate', :via => :get, :constraints => { :unique_key => /~.+/ }
