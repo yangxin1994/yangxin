fe179a852ea4637fbb7d52efdf87006e0a241fef
Davis Gao <naitnix@126.com>
2014-10-27 09:56:59 +0800

    modified

--- app/assets/javascripts/quill/views/fillers_mobile/base.js
+++ app/assets/javascripts/quill/views/fillers_mobile/base.js
@@ -73,8 +73,13 @@ $(function(){
 			this.$el.addClass('q-' + quill.helpers.QuestionType.getName(this.model.get('question_type')).toLowerCase());
 
 			var info = this.model.getInfo();
-			if (this.model.get('question_type') == 12){
-				info = info.replace(/将右侧选项拖拽至左侧空白处进行排列/,'点击右侧向上或向下箭头进行排序')
+			if (this.model.get('question_type') == 12){ // 住建委项目特殊添加
+				info = info.replace(/将右侧选项拖拽至左侧空白处进行排列/,'点击右侧向上或向下箭头进行排序');
+			}
+
+			if (this.model.get('question_type') == 1){ // 住建委项目特殊添加
+				info = info.replace(/矩阵选择题/,'单选题');
+				info = info.replace(/每行/,'每题');
 			}
 
 			if(!info)
