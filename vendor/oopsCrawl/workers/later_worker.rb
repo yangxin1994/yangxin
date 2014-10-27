24d7e349dda5dd85de54361ab3c6799955411ab6
Davis Gao <naitnix@126.com>
2014-10-27 16:44:10 +0800

    modified

--- vendor/oopsCrawl/workers/later_worker.rb
+++ vendor/oopsCrawl/workers/later_worker.rb
@@ -29,4 +29,4 @@ class LaterWorker
 end
 
 # Sidekiq::Cron::Job.create( name: 'Crawling Douban nowplaying - every 1 day', cron: '57 11 * * *', klass: 'NowplayingWorker')
-Sidekiq::Cron::Job.create( name: 'Crawling Douban later - every 1 day', cron: '10 23 * * *', klass: 'LaterWorker')
+Sidekiq::Cron::Job.create( name: 'Crawling Douban later - every 12 hours', cron: '* */12 * * *', klass: 'LaterWorker')
