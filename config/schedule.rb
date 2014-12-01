1e538f75c5a44b742afbe247fc8039b4ebf5dac6
oliverhuang <oliverhdh@gmail.com>
2014-12-01 17:33:20 +0800

    update schedule

--- config/schedule.rb
+++ config/schedule.rb
@@ -27,10 +27,6 @@ every 1.days, :at => '4:30 pm' do
 	runner "SmsInvitationWorker.perform_async"
 end
 
-every 1.days do
-	command "cd ~/db_bak/; mongodump -d oops_data_production -o './' -u oopsdata -password=o2psllyscdata; tar -zcf oops_data_production_$(date +%d-%m-%y).tar.gz oops_data_production; rm -rf oops_data_production"
-end
-
 every 1.weeks do
 	runner "SampleAttribute.make_statistics"
 end
