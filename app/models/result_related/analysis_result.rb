112fa93192174b68b235207988e443cbf8592c3d
Davis Gao <naitnix@126.com>
2015-01-05 15:44:29 +0800

    m

--- app/models/result_related/analysis_result.rb
+++ app/models/result_related/analysis_result.rb
@@ -1,3 +1,4 @@
+# encoding: utf-8
 require 'error_enum'
 require 'array'
 require 'tool'
--- app/workers/analysis_worker.rb
+++ app/workers/analysis_worker.rb
@@ -1,3 +1,4 @@
+# encoding: utf-8
 class AnalysisWorker
   include Sidekiq::Worker
   sidekiq_options :retry => false, :queue => "oopsdata_#{Rails.env}".to_sym
