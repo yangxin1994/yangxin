9a83be52c97c1f55dfc8d965deb5732d45796694
Davis Gao <naitnix@126.com>
2015-02-05 16:05:19 +0800

    m

--- app/controllers/travel/travel_controller.rb
+++ app/controllers/travel/travel_controller.rb
@@ -1,5 +1,5 @@
 class Travel::TravelController < ApplicationController
-	#before_filter :require_travel_sign_in,:except => [:login]
-	#before_filter :require_supervisor	
+	before_filter :require_travel_sign_in,:except => [:login]
+	before_filter :require_supervisor,:except => [:login]	
 	layout "travel"
 end
