bdea00056e9399551b2046f971b3ff0b45ff242e
Davis Gao <naitnix@126.com>
2015-02-06 18:28:17 +0800

    m

--- app/controllers/travel/cities_controller.rb
+++ app/controllers/travel/cities_controller.rb
@@ -74,13 +74,12 @@ class Travel::CitiesController < Travel::TravelController
 				survey.write_attributes(amount:interviewer_tasks.map{|t| t.quota['rules'][0]['amount']}.inject{|sum,x| sum + x})
 				survey.write_attributes(finish:interviewer_tasks.map{|t| t.quota['submitted_count']}.inject{|sum,x| sum + x})
 				survey.write_attributes(suffice:interviewer_tasks.map{|t| t.quota['finished_count']}.inject{|sum,x| sum + x})
-				interviewer_tasks = interviewer_tasks.map do |t|
+				interviewer_tasks = interviewer_tasks.map! do |t|
 					t.write_attributes(nickname:t.user.nickname)
 					finish_percent = ( ( (t.quota['submitted_count'] / t.quota['rules'][0]['amount'].to_f) * 100 ).to_s + '%' )
 					suffice_percent = ( ( (t.quota['finished_count'] / t.quota['rules'][0]['amount'].to_f) * 100 ).to_s + '%' )
 					t.write_attributes(finish_percent:finish_percent)
 					t.write_attributes(suffice_percent:suffice_percent)
-					t
 				end
 				survey.write_attributes(interviews:interviewer_tasks)
 				@surveys << task.survey  if !@surveys.include?(survey)
