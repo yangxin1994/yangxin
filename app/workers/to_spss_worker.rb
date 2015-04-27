9c416b236aee6687ab64892385e7e2c5e783a571
oliverhuang <oliverhdh@gmail.com>
2015-04-27 16:04:37 +0800

    update

--- app/workers/to_spss_worker.rb
+++ app/workers/to_spss_worker.rb
@@ -12,20 +12,29 @@ class ToSpssWorker
     puts("answers_length:-----------#{answers.length}")
     # generate result key
     result_key = ExportResult.generate_spss_result_key(survey.last_update_time,answers)
+    puts '1'
     existing_export_result = ExportResult.find_by_result_key(result_key)
+    puts '2'
     if existing_export_result.nil?
+      puts '3'
       # create new result record
       export_result = ExportResult.create(:result_key => result_key,:task_id => task_id)
+      puts export_result.inspect
     else
+      puts '4'
       export_result = ExportResult.create(
         :result_key => result_key,
         :task_id => task_id,
         :ref_result_id => existing_export_result._id
       )
+      puts export_result.inspect
       return true
     end
+    puts '5'
     survey.export_results << export_result
+    puts '6'
     retval = export_result.generate_spss(survey, answers, result_key)
+    puts '7'
     return true
   end
 end
