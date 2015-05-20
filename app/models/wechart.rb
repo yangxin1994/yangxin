2ac172614dae20a20c08a5c1772c6acf02507f31
Davis Gao <naitnix@126.com>
2015-05-20 13:17:18 +0800

    debug

--- app/models/wechart.rb
+++ app/models/wechart.rb
@@ -161,7 +161,7 @@ class Wechart
     Net::HTTP.start(uri.host,uri.port,use_ssl:true,ca_file:Rails.root.to_s + '/rootca.pem',key:p12.key,cert:p12.certificate) do |http|
       request  = Net::HTTP::Post.new(uri.path)
       request.body = wechat_hash
-      request.content_type = 'text/xml'
+      request.content_type = 'text/xml;charset=UTF-8'
       response = http.request(request)
       Rails.logger.info '-------------------------------------'
       Rails.logger.info  response.body
