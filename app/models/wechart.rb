5bf920fa59b863ba4e6e371c23d5aae6046dc67e
Davis Gao <naitnix@126.com>
2015-05-20 18:32:34 +0800

    debug

--- app/models/wechart.rb
+++ app/models/wechart.rb
@@ -151,10 +151,6 @@ class Wechart
 
     wechat_hash = builder.to_xml
 
-    Rails.logger.info '*******************************'
-    Rails.logger.info wechat_hash
-    Rails.logger.info '*******************************'
-
 
     p12 = OpenSSL::PKCS12.new(File.read(Rails.root.to_s + '/apiclient_cert.p12'), "#{Wechart.mch_id}")
     uri = URI.parse("https://api.mch.weixin.qq.com/mmpaymkttransfers/sendredpack")
@@ -162,26 +158,12 @@ class Wechart
     Net::HTTP.start(uri.host,uri.port,use_ssl:true,ca_file:Rails.root.to_s + '/rootca.pem',key:p12.key,cert:p12.certificate) do |http|
       request  = Net::HTTP::Post.new(uri.path)
       request.body = wechat_hash
-      # request.content_type = 'text/xml;charset=utf-8'
       response = http.request(request)
-      Rails.logger.info '&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&'
-      Rails.logger.info request.body.inspect
-      Rails.logger.info '&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&'
+      res      = Nokogiri::HTML(response.body)
       Rails.logger.info '-------------------------------------'
-      Rails.logger.info  response.body
+      Rails.logger.info  res.inspect
       Rails.logger.info '-------------------------------------'       
     end
-    # https.use_ssl = true
-    # pem = File.read(Rails.root.to_s + '/rootca.pem')  
-    # https.cert = OpenSSL::X509::Certificate.new(pem)
-    # https.cert = 
-    # https.key = OpenSSL::PKey::RSA.new(pem)  
-    # https.ca_file = OpenSSL::PKCS12.new(File.read(Rails.root.to_s + '/apiclient_cert.p12'), "#{Wechart.mch_id}")
-    # https.verify_mode = OpenSSL::SSL::VERIFY_PEER
-    # req = Net::HTTP::Post.new(uri.path)
-    # req.body = wechat_hash
-    # res = https.request(req)
-
   end 
 end
 
