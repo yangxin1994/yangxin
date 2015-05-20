c85ad959a6e7d8759124542a13fcbc4f508e235a
Davis Gao <naitnix@126.com>
2015-05-20 09:31:01 +0800

    debug

--- app/models/wechart.rb
+++ app/models/wechart.rb
@@ -157,7 +157,7 @@ class Wechart
     uri = URI.parse("https://api.mch.weixin.qq.com/mmpaymkttransfers/sendredpack")
     https = Net::HTTP.new(uri.host,uri.port)
     https.use_ssl = true
-    https.ca_file = Rails.root.to_s + '/apiclient_cert.p12'
+    https.ca_file = OpenSSL::PKCS12.new(File.read(Rails.root.to_s + '/apiclient_cert.p12'), "#{Wechart.mch_id}")
     https.verify_mode = OpenSSL::SSL::VERIFY_PEER
     req = Net::HTTP::Post.new(uri.path)
     req.body = wechat_hash
