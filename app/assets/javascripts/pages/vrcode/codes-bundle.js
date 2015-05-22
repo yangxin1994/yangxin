5949f97a756cbbc732d09b0fa6a4fdb44858c222
yanhao <yanhao@oopsdata.com>
2015-05-22 13:31:19 +0800

    1111

--- app/assets/javascripts/pages/vrcode/codes-bundle.js
+++ app/assets/javascripts/pages/vrcode/codes-bundle.js
@@ -1,12 +1,11 @@
 //(function($){
 
 	function submitvrcode(){
-		//document.getElementsById("edit_user_51aea34deb0e5b571f000079").submit();
-		var img_id  = $("#image_id").val();
-		var remote_ip= $("#remote_ip").val();
-		var code = $("#code").val();
-		$.post("/vrcode/codes",{img_id: img_id, remote_ip:remote_ip, code:code}, function(data){
+		$.post("/vrcode/codes",{img_id: $("#image_id").val(), remote_ip:$("#remote_ip").val(), code:$("#code").val()}, function(data){
 			console.log(data)
+			$("#image_id").val(data.value.id);
+			$("#remote_ip").val(data.value.ip);
+			$(".vrcode>img").attr("src",data.value.url)
 		},"json");
 	}
 
