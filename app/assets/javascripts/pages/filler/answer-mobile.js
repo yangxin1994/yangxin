508f01fbef60bd47d5be39d7ff69cd6b521dae55
Davis Gao <naitnix@126.com>
2015-05-25 12:21:39 +0800

    debug

--- app/assets/javascripts/pages/filler/answer-mobile.js
+++ app/assets/javascripts/pages/filler/answer-mobile.js
@@ -6,21 +6,6 @@
 jQuery(function($) {
 	//微信红包问卷
 	if(window.is_wechart){
-		window.appid     	= <%= raw (@appid).to_json %>;
-		window.noncestr  	= <%= raw (@noncestr).to_json %>;    
-		window.timestamp 	= <%= raw (@timestamp).to_json %>;
-		window.url       	= <%= raw (@url).to_json %>;
-		window.signure   	= <%= raw (@signure).to_json %>;
-		window.share_link   = window.location.origin + '/s/' + <%= raw @reward_scheme_id.to_json %>;
-		wx.config({
-		    debug: true,
-		    appId: window.appid,
-		    timestamp: window.timestamp,
-		    nonceStr: window.noncestr,
-		    signature: window.signure,
-		    jsApiList: ['onMenuShareTimeline','onMenuShareAppMessage','hideMenuItems']
-		})
-
 		wx.ready(function(){
 			arr = ["menuItem:share:qq","menuItem:share:weiboApp","menuItem:favorite","menuItem:share:facebook","menuItem:share:QZone",
 				   "menuItem:editTag","menuItem:editTag","menuItem:delete","menuItem:copyUrl","menuItem:originPage","menuItem:readMode",
