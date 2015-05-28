7afeaec7eae34aa6c2fba8133a2056531b06043d
Davis Gao <naitnix@126.com>
2015-05-28 08:49:21 +0800

    fix bug

--- app/assets/javascripts/pages/vrcode/codes-bundle.js
+++ app/assets/javascripts/pages/vrcode/codes-bundle.js
@@ -2,8 +2,6 @@
 
 	function submitvrcode(){
 		$.post("/vrcode/codes",{img_id: $("#image_id").val(), remote_ip:$("#remote_ip").val(), code:$("#code").val()}, function(data){
-			console.log(data)
-			alert("Your answer has been submitted.  Please fill the next~~~");
 			$("#image_id").val(data.value.id);
 			$("#remote_ip").val(data.value.ip);
 			$(".vrcode>img").attr("src",data.value.url)
