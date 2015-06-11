b2631e1ebde557ae6eaf261ba2fd9c1089475aea
Davis Gao <naitnix@126.com>
2015-06-11 13:44:42 +0800

    debug

--- app/assets/javascripts/pages/filler/answer-mobile.js
+++ app/assets/javascripts/pages/filler/answer-mobile.js
@@ -4,16 +4,14 @@
 //=require weixin
 
 jQuery(function($) {
+	arr = ["menuItem:share:qq","menuItem:share:weiboApp","menuItem:favorite","menuItem:share:facebook","menuItem:share:QZone","menuItem:editTag","menuItem:editTag","menuItem:delete","menuItem:copyUrl","menuItem:originPage","menuItem:readMode","menuItem:openWithQQBrowser","menuItem:openWithSafari","menuItem:share:email","menuItem:share:brand"]
+
 	//微信红包问卷
 	if(window.is_wechart){
 		wx.ready(function(){
-			arr = ["menuItem:share:qq","menuItem:share:weiboApp","menuItem:favorite","menuItem:share:facebook","menuItem:share:QZone",
-				   "menuItem:editTag","menuItem:editTag","menuItem:delete","menuItem:copyUrl","menuItem:originPage","menuItem:readMode",
-				   "menuItem:openWithQQBrowser","menuItem:openWithSafari","menuItem:share:email","menuItem:share:brand"]
 			wx.hideMenuItems({
 			    menuList: arr
 			});
-	
 			wx.onMenuShareTimeline({
 			    title: '答问卷领红包,大家快来抢!',
 			    link: window.share_link,
@@ -27,7 +25,24 @@ jQuery(function($) {
 			    imgUrl: window.location.origin + '/assets/od-quillme/favicon.ico'
 			});
 		})
-		
+	}else{
+		wx.ready(function(){
+			wx.hideMenuItems({
+			    menuList: arr
+			});
+			wx.onMenuShareTimeline({
+			    title: window.survey_title,
+			    link: window.share_link,
+			    imgUrl: window.location.origin + '/assets/od-quillme/favicon.ico'
+			});
+	
+			wx.onMenuShareAppMessage({
+			    title: window.survey_title,
+			    desc: '大家一起来答问卷吧',
+			    link: window.share_link,
+			    imgUrl: window.location.origin + '/assets/od-quillme/favicon.ico'
+			});
+		})
 	}
 });
 
